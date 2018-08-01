d0529d3e61961dbdd7b32e054cc1eea09c00e717
Brandon Chinn <brandon@leapyear.io>
2018-08-01 12:13:23 -0700

    Implement execMerge

--- core/src/MergeBot.hs
+++ core/src/MergeBot.hs
@@ -6,11 +6,21 @@ Portability :  portable
 
 Defines the core functionality of the merge bot.
 -}
+{-# LANGUAGE RecordWildCards #-}
 
 module MergeBot
   ( execMerge
   ) where
 
+import MergeBot.Monad (MonadGHBranch(..))
+import MergeBot.State (BotState, clearMergeJobs)
+
 -- | Execute a merge after a successful CI run.
-execMerge :: MonadGHBranch m => BotConfig -> BotState -> m BotState
-execMerge = undefined
+execMerge :: MonadGHBranch m => BotState -> m (Either String BotState)
+execMerge state = do
+  canPromote <- canPromoteStaging
+  if canPromote
+    then do
+      promoteStaging
+      return $ Right $ clearMergeJobs state
+    else return $ Left "Could not promote 'staging' to 'master'"
