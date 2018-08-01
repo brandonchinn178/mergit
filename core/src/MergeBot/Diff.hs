1cf93af172adcaef3a33d50e484e2fecf04ad5cd
Brandon Chinn <brandon@leapyear.io>
2018-08-01 13:17:45 -0700

    Make DiffOptions optional (#5)
    * Make DiffOptions optional
    
    * Set DiffOptions in BotConfig

--- core/src/MergeBot/Diff.hs
+++ core/src/MergeBot/Diff.hs
@@ -9,11 +9,21 @@ Defines the Diff data type, representing a GitHub pull request.
 Note on naming: a pull request can be thought of as a code diff
 trying to be merged into the main codebase.
 -}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module MergeBot.Diff
   ( DiffId
-  , DiffOption(..)
+  , DiffOptions(..)
+  , DiffOptionsPartial
+  , DiffOptionsFull
+  , resolveOptions
   ) where
+
+import Data.Functor.Identity (Identity)
+import Data.Maybe (fromMaybe)
 
 import MergeBot.Merge (MergeAlgorithm)
 
@@ -21,6 +31,25 @@ import MergeBot.Merge (MergeAlgorithm)
 type DiffId = Int
 
 -- | Options specified per-PR to customize merge bot behavior.
-data DiffOption
-  = MergeAlgorithm MergeAlgorithm
-  deriving (Show,Eq)
+data DiffOptions f = DiffOptions
+  { defaultMergeAlgorithm :: HKD f MergeAlgorithm
+  }
+
+-- | Strip out Identity applications.
+type family HKD f a where
+  HKD Identity a = a
+  HKD f a = f a
+
+type DiffOptionsPartial = DiffOptions Maybe
+deriving instance Show DiffOptionsPartial
+deriving instance Eq DiffOptionsPartial
+
+type DiffOptionsFull = DiffOptions Identity
+deriving instance Show DiffOptionsFull
+deriving instance Eq DiffOptionsFull
+
+-- | Resolve the given options using the given defaults.
+resolveOptions :: DiffOptionsPartial -> DiffOptionsFull -> DiffOptionsFull
+resolveOptions partial full = DiffOptions
+  { defaultMergeAlgorithm = fromMaybe (defaultMergeAlgorithm full) (defaultMergeAlgorithm partial)
+  }
