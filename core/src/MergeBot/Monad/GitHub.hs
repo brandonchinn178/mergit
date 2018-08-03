b7f932f07769bc65f1ac2a0f09300e390fe544c0
Brandon Chinn <brandon@leapyear.io>
2018-08-03 14:03:24 -0700

    Add POSTing to GitHub API

--- core/src/MergeBot/Monad/GitHub.hs
+++ core/src/MergeBot/Monad/GitHub.hs
@@ -14,17 +14,20 @@ module MergeBot.Monad.GitHub
   ( GitHubT
   , runGitHubT
   , fromGitHub
+  , toGitHub
   ) where
 
 import Control.Lens (view, (&), (.~), (?~))
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Reader (MonadReader, ReaderT, ask, runReaderT)
-import Data.Aeson (Value, eitherDecode)
+import Data.Aeson (Value(..), eitherDecode)
+import Data.ByteString.Lazy (ByteString)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
-import Network.Wreq (auth, basicAuth, defaults, header, responseBody)
-import Network.Wreq.Session (Session, getWith, newSession)
+import Network.Wreq
+    (Options, Response, auth, basicAuth, defaults, header, responseBody)
+import Network.Wreq.Session (Session, getWith, newSession, postWith)
 import System.Environment (getEnv)
 
 import MergeBot.Monad.Class
@@ -32,7 +35,7 @@ import MergeBot.Monad.Class
 data GitHubConfig = GitHubConfig
   { ghUsername :: Text
   , ghPassword :: Text
-  , ghSession :: Session
+  , ghSession  :: Session
   }
 
 newtype GitHubT m a = GitHubT { unGitHubT :: ReaderT GitHubConfig m a }
@@ -63,8 +66,22 @@ instance MonadIO m => MonadGHBranch (GitHubT m) where
 instance MonadIO m => MonadGHPullRequest (GitHubT m) where
   mergePullRequest _ _ = GitHubT $ liftIO $ putStrLn "mergePullRequest"
 
+{- Connecting to the GitHub API directly -}
+
 -- | The GitHub API endpoint with question marks that can be replaced by passed-in values.
 type Endpoint = Text
+
+-- | Sends a GET request to the GitHub API.
+fromGitHub :: MonadIO m => Endpoint -> [Text] -> GitHubT m Value
+fromGitHub = githubAPI getWith
+
+-- | Sends a POST request to the GitHub API.
+toGitHub :: MonadIO m => Endpoint -> [Text] -> Value -> GitHubT m Value
+toGitHub endpoint values postData = githubAPI postWith' endpoint values
+  where
+    postWith' opts session url = postWith opts session url postData
+
+{- Helpers -}
 
 -- | Interpolate the given values into the given endpoint with question marks.
 populateEndpoint :: Endpoint -> [Text] -> Either String Text
@@ -76,14 +93,18 @@ populateEndpoint endpoint values =
     split = Text.splitOn "?" endpoint
     interleave xs ys = concat $ zipWith (\a b -> [a, b]) xs ys
 
--- | Sends a GET request to the GitHub API.
-fromGitHub :: MonadIO m => Endpoint -> [Text] -> GitHubT m Value
-fromGitHub endpoint values = do
+-- | A helper function to connect to the GitHub API.
+githubAPI :: MonadIO m
+  => (Options -> Session -> String -> IO (Response ByteString))
+  -> Endpoint
+  -> [Text]
+  -> GitHubT m Value
+githubAPI httpWith endpoint values = do
   GitHubConfig{..} <- ask
   let opts' = opts & auth ?~ basicAuth (Text.encodeUtf8 ghUsername) (Text.encodeUtf8 ghPassword)
 
   endpoint' <- either fail return $ populateEndpoint endpoint values
-  liftIO $ decode' . view responseBody =<< getWith opts' ghSession (url ++ Text.unpack endpoint')
+  liftIO $ decode' . view responseBody =<< httpWith opts' ghSession (url ++ Text.unpack endpoint')
   where
     decode' = either fail return . eitherDecode
     opts = defaults
