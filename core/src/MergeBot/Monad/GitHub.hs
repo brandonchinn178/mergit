c07cae09ec4266c4abffaf0fd0d89936f74b25cf
Brandon Chinn <brandon@leapyear.io>
2018-08-09 16:39:08 -0700

    Better UX with calling GitHub API

--- core/src/MergeBot/Monad/GitHub.hs
+++ core/src/MergeBot/Monad/GitHub.hs
@@ -14,42 +14,53 @@ Defines monad definitions for the GitHub API.
 module MergeBot.Monad.GitHub
   ( GitHubT
   , runGitHubT
-  , GitHubData(..)
+  , KeyValue(..)
+  , (.:)
   -- * Low-level operations
-  , getGitHub
-  , postGitHub
-  , deleteGitHub
+  , github
+  , github_
+  , githubWith
+  , githubWith_
   , handleStatus
   ) where
 
-import Control.Lens (view, (&), (.~), (?~), (^.))
 import Control.Monad (void)
 import Control.Monad.Catch (MonadCatch, MonadThrow, handleJust)
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Reader (MonadReader, ReaderT, ask, runReaderT)
 import Data.Aeson
-    (FromJSON, ToJSON(..), Value(..), eitherDecode, object, withObject)
+    (FromJSON, ToJSON(..), Value(..), eitherDecode, encode, object, withObject)
 import Data.Aeson.Types (parseEither, parseField)
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as ByteString
 import Data.Monoid ((<>))
+import Data.Ratio (denominator, numerator)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
-import Network.HTTP.Client (HttpException(..), HttpExceptionContent(..))
-import Network.HTTP.Types (Status, status404)
-import Network.Wreq
-    ( Options
-    , Response
-    , auth
-    , basicAuth
-    , defaults
-    , header
+import Network.HTTP.Client
+    ( HttpException(..)
+    , HttpExceptionContent(..)
+    , Manager
+    , Request(..)
+    , RequestBody(..)
+    , applyBasicAuth
+    , httpLbs
+    , newManager
+    , parseRequest_
     , responseBody
     , responseStatus
+    , throwErrorStatusCodes
     )
-import Network.Wreq.Session
-    (Session, deleteWith, getWith, newSession, postWith, putWith)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Types
+    ( Status
+    , StdMethod(..)
+    , hAccept
+    , hUserAgent
+    , renderStdMethod
+    , status404
+    )
 import System.Environment (getEnv)
 
 import MergeBot.Monad.Class
@@ -59,7 +70,7 @@ data GitHubConfig = GitHubConfig
   , ghRepo     :: Text
   , ghUsername :: Text
   , ghPassword :: Text
-  , ghSession  :: Session
+  , ghManager  :: Manager
   }
 
 newtype GitHubT m a = GitHubT { unGitHubT :: ReaderT GitHubConfig m a }
@@ -81,34 +92,30 @@ runGitHubT m = do
       ghRepo = "merge-bot-test"
   ghUsername <- getEnv' "MERGEBOT_USERNAME"
   ghPassword <- getEnv' "MERGEBOT_PASSWORD"
-  ghSession <- liftIO newSession
+  ghManager <- liftIO $ newManager tlsManagerSettings
   runReaderT (unGitHubT m) GitHubConfig{..}
   where
     getEnv' = liftIO . fmap Text.pack . getEnv
 
 instance (MonadCatch m, MonadIO m) => MonadGHBranch (GitHubT m) where
-  getBranch diffId = do
-    GitHubConfig{..} <- ask
-    let getRef diff = diff .: "head" .: "ref"
-    either (const Nothing) (Just . getRef) <$> handleStatus status404
-      (getGitHub "/repos/:owner/:repo/pulls/:number" [ghOwner, ghRepo, showT diffId])
+  getBranch patchId = fmap toRef $ handleStatus status404 $
+    github GET "/repos/:owner/:repo/pulls/:number" ["number" :=* patchId]
+    where
+      toRef = either (const Nothing) (Just . getRef)
+      getRef patch = patch .: "head" .: "ref"
 
   createBranch branch = do
-    GitHubConfig{..} <- ask
-    master <- getGitHub "/repos/:owner/:repo/git/refs/:ref" [ghOwner, ghRepo, "heads/master"]
-    let sha = master .: "object" .: "sha" :: Text
-    postGitHub "/repos/:owner/:repo/git/refs" [ghOwner, ghRepo]
+    master <- github GET "/repos/:owner/:repo/git/refs/:ref" ["ref" := "heads/master"]
+    githubWith_ POST "/repos/:owner/:repo/git/refs" []
       [ "ref" := "refs/heads/" <> branch
-      , "sha" := sha
+      , "sha" := master .: "object" .: "sha"
       ]
 
-  deleteBranch branch = do
-    GitHubConfig{..} <- ask
-    deleteGitHub "/repos/:owner/:repo/git/refs/:ref" [ghOwner, ghRepo, "heads/" <> branch]
+  deleteBranch branch =
+    github_ DELETE "/repos/:owner/:repo/git/refs/:ref" ["ref" := "heads/" <> branch]
 
-  mergeBranch base branch = do
-    GitHubConfig{..} <- ask
-    postGitHub "/repos/:owner/:repo/merges" [ghOwner, ghRepo]
+  mergeBranch base branch =
+    githubWith_ POST "/repos/:owner/:repo/merges" []
       [ "base" := base
       , "head" := branch
       , "commit_message" := Text.unwords ["[lybot] Merge branch", branch, "into", base]
@@ -116,18 +123,14 @@ instance (MonadCatch m, MonadIO m) => MonadGHBranch (GitHubT m) where
 
 instance (MonadIO m, MonadCatch m) => MonadGHPullRequest (GitHubT m) where
   mergePullRequest patchId mergeAlgorithm = do
-    GitHubConfig{..} <- ask
-    patch <- getGitHub "/repos/:owner/:repo/pulls/:number" [ghOwner, ghRepo, showT patchId]
-    let sha = patch .: "head" .: "sha" :: Text
-        patchName = patch .: "title" :: Text
-        branch = patch .: "head" .: "ref" :: Text
-    putGitHub "/repos/:owner/:repo/pulls/:number/merge" [ghOwner, ghRepo, showT patchId]
+    patch <- github GET "/repos/:owner/:repo/pulls/:number" ["number" :=* patchId]
+    githubWith_ PUT "/repos/:owner/:repo/pulls/:number/merge" ["number" :=* patchId]
       [ "commit_title" := "[lybot] Merge #" <> showT patchId
-      , "commit_message" := patchName
-      , "sha" := sha
+      , "commit_message" := patch .: "title"
+      , "sha" := patch .: "head" .: "sha"
       , "merge_method" := showT mergeAlgorithm
       ]
-    deleteBranch branch
+    deleteBranch $ patch .: "head" .: "ref"
 
 {- Connecting to the GitHub API directly -}
 
@@ -135,61 +138,76 @@ instance (MonadIO m, MonadCatch m) => MonadGHPullRequest (GitHubT m) where
 -- passed-in values.
 type Endpoint = Text
 
--- | Sends a GET request to the GitHub API.
-getGitHub :: MonadIO m => Endpoint -> [Text] -> GitHubT m Value
-getGitHub = githubAPI getWith
+-- | Mapping to populate the endpoint.
+type EndpointVals = [KeyValue]
 
--- | Sends a POST request to the GitHub API.
-postGitHub :: MonadIO m => Endpoint -> [Text] -> [GitHubData] -> GitHubT m ()
-postGitHub endpoint values postData = void $ githubAPI postWith' endpoint values
-  where
-    postWith' opts session url = postWith opts session url $ fromData postData
+-- | Data to send to GitHub.
+type GitHubData = [KeyValue]
 
--- | Sends a DELETE request to the GitHub API.
-deleteGitHub :: MonadIO m => Endpoint -> [Text] -> GitHubT m ()
-deleteGitHub endpoint = void . githubAPI deleteWith endpoint
+-- | Access the GitHub API with the given method.
+github :: MonadIO m => StdMethod -> Endpoint -> EndpointVals -> GitHubT m Value
+github method endpoint vals = githubAPI Nothing method endpoint vals
 
--- | Sends a PUT request to the GitHub API.
-putGitHub :: MonadIO m => Endpoint -> [Text] -> [GitHubData] -> GitHubT m ()
-putGitHub endpoint values putData = void $ githubAPI putWith' endpoint values
-  where
-    putWith' opts session url = putWith opts session url $ fromData putData
+-- | 'github' but ignoring the result.
+github_ :: MonadIO m => StdMethod -> Endpoint -> EndpointVals -> GitHubT m ()
+github_ method endpoint vals = void $ github method endpoint vals
+
+-- | Access the GitHub API with the given method, sending data in the request body.
+githubWith :: MonadIO m => StdMethod -> Endpoint -> EndpointVals -> GitHubData -> GitHubT m Value
+githubWith method endpoint vals ghData = githubAPI (Just ghData) method endpoint vals
+
+-- | 'githubWith', but ignoring the result.
+githubWith_ :: MonadIO m => StdMethod -> Endpoint -> EndpointVals -> GitHubData -> GitHubT m ()
+githubWith_ method endpoint vals ghData = void $ githubWith method endpoint vals ghData
 
 {- API Helpers -}
 
 -- | Set the placeholders in the given endpoint to the given values.
-populateEndpoint :: Endpoint -> [Text] -> Text
-populateEndpoint endpoint values =
-  Text.intercalate "/" $ populate (Text.splitOn "/" endpoint) values
+populateEndpoint :: EndpointVals -> Endpoint -> Text
+populateEndpoint values endpoint = Text.intercalate "/" . map populate . Text.splitOn "/" $ endpoint
   where
-    populate [] [] = []
-    populate [] _ = fail' "Too many values passed into endpoint"
-    populate (x:xs) vs =
-      if ":" `Text.isPrefixOf` x
-        then case vs of
-          [] -> fail' "Not enough values passed into endpoint"
-          v:vs' -> v : populate xs vs'
-        else x : populate xs vs
-    fail' msg = error $ Text.unpack $ msg <> ": " <> endpoint
+    values' = map kvToText values
+    populate t = case Text.uncons t of
+      Nothing -> t
+      Just (':', key) -> case lookup key values' of
+        Nothing -> fail' $ "Could not find value for key '" <> key <> "'"
+        Just v -> v
+      Just _ -> t
+    fail' msg = error . Text.unpack $ msg <> ": " <> endpoint
 
 -- | A helper function to connect to the GitHub API.
 githubAPI :: MonadIO m
-  => (Options -> Session -> String -> IO (Response ByteString))
+  => Maybe GitHubData
+  -> StdMethod
   -> Endpoint
-  -> [Text]
+  -> EndpointVals
   -> GitHubT m Value
-githubAPI httpWith endpoint values = do
+githubAPI maybeData method endpoint vals = do
   GitHubConfig{..} <- ask
-  let opts' = opts & auth ?~ basicAuth (Text.encodeUtf8 ghUsername) (Text.encodeUtf8 ghPassword)
+  let vals' = vals ++ ["owner" := ghOwner, "repo" := ghRepo]
+      endpoint' = populateEndpoint vals' endpoint
+      url' = Text.unpack $ "https://api.github.com" <> endpoint'
+      username = Text.encodeUtf8 ghUsername
+      password = Text.encodeUtf8 ghPassword
+      request = applyBasicAuth username password $ parseRequest_ url'
 
-  response <- liftIO $ view responseBody <$> httpWith opts' ghSession url
+  response <- httpLbs' ghManager request
+    { method = renderStdMethod method
+    , requestHeaders =
+        (hAccept, "application/vnd.github.v3+json")
+        : (hUserAgent, "lybot")
+        : requestHeaders request
+    , requestBody = RequestBodyLBS $ case maybeData of
+        Nothing -> ByteString.empty
+        Just d -> encode $ kvToValue d
+    , checkResponse = throwErrorStatusCodes
+    }
+
   if ByteString.null response
     then return $ object []
     else decode' response
   where
-    opts = defaults
-      & header "Accept" .~ ["application/vnd.github.v3+json"]
-    url = Text.unpack $ "https://api.github.com" <> populateEndpoint endpoint values
+    httpLbs' manager request = liftIO $ responseBody <$> httpLbs request manager
 
 -- | Handle the given status code, returning Left if the error was given and Right if not, and
 -- the response body for each.
@@ -198,7 +216,7 @@ handleStatus status = handleJust statusException (fmap Left . decodeFromStrict) 
   where
     decodeFromStrict = decode' . ByteString.fromStrict
     statusException (HttpExceptionRequest _ (StatusCodeException r body))
-      | r ^. responseStatus == status = Just body
+      | responseStatus r == status = Just body
     statusException _ = Nothing
 
 {- JSON helpers -}
@@ -206,14 +224,33 @@ handleStatus status = handleJust statusException (fmap Left . decodeFromStrict) 
 decode' :: (FromJSON a, Monad m) => ByteString -> m a
 decode' = either fail return . eitherDecode
 
--- | Data key-value pairs that can be sent to GitHub.
-data GitHubData where
-  (:=) :: ToJSON v => Text -> v -> GitHubData
-infixl 1 :=
+data KeyValue where
+  (:=) :: Text -> Text -> KeyValue
+  (:=*) :: (Show v, ToJSON v) => Text -> v -> KeyValue
+infixr 1 :=
+infixr 1 :=*
 
--- | Convert the given GitHubData into a JSON value to send to GitHub.
-fromData :: [GitHubData] -> Value
-fromData = object . map (\(k := v) -> (k, toJSON v))
+-- | Convert the given KeyValues into a JSON Object.
+kvToValue :: [KeyValue] -> Value
+kvToValue = object . map toPair
+  where
+    toPair (k := v) = (k, String v)
+    toPair (k :=* v) = (k, toJSON v)
+
+-- | Represent the given KeyValue as a pair of Texts.
+kvToText :: KeyValue -> (Text, Text)
+kvToText (k := v) = (k, v)
+kvToText (k :=* v) = (k, v')
+  where
+    v' = case toJSON v of
+      String t -> t
+      Number scientific ->
+        let rational = toRational scientific
+        in if denominator rational == 1
+          then showT $ numerator rational
+          else showT (realToFrac rational :: Double)
+      Bool b -> showT b
+      _ -> error $ "Could not convert value: " ++ show v
 
 (.:) :: FromJSON a => Value -> Text -> a
 (.:) v key = either error id $ parseEither parseObject v
