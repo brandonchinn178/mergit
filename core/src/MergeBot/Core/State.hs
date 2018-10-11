1dc201996965bda4d214e50b6ea5d35fbf90f940
Brandon Chinn <brandon@leapyear.io>
2018-10-11 16:15:49 -0700

    State only contains merge queue

--- core/src/MergeBot/Core/State.hs
+++ core/src/MergeBot/Core/State.hs
@@ -6,76 +6,45 @@ Portability :  portable
 
 Defines the data type representing the internal state of the merge bot.
 -}
+{-# LANGUAGE RecordWildCards #-}
 
 module MergeBot.Core.State
-  ( BotState
+  ( MergeQueue
+  , BotState
   , newBotState
-  -- Queries
-  , getMergeJobs
-  -- Mutations
+  , getMergeQueue
+  , clearMergeQueue
   , insertMergeQueue
   , removeMergeQueue
-  , clearMergeJobs
-  , initMergeJob
-  , startTryJob
   ) where
 
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
 import Data.Set (Set)
+import qualified Data.Set as Set
+import GitHub.Data.Id (Id)
+import GitHub.Data.PullRequests (PullRequest)
 
-import MergeBot.Core.Patch (PatchId)
-
-{- PatchState -}
-
-data PatchState
-  = TryJob
-  | MergeQueue
-  | MergeJob
-  deriving (Show,Eq)
-
-{- BotState -}
+type MergeQueue = Set (Id PullRequest)
 
 -- | The state of the merge bot.
-newtype BotState = BotState (Map PatchId PatchState)
-  deriving (Show)
+--
+-- Most of the merge bot should be stateless; everything is either retrieved from the GitHub API
+-- or stored in-memory. Could also cache replies from the GitHub API, but we should NOT be storing
+-- persisted data anywhere.
+newtype BotState = BotState
+  { mergeQueue :: MergeQueue
+  } deriving (Show)
 
 newBotState :: BotState
-newBotState = BotState Map.empty
+newBotState = BotState Set.empty
 
-{- Querying state -}
+getMergeQueue :: BotState -> MergeQueue
+getMergeQueue = mergeQueue
 
--- | Get all the patchs in the merge job list.
-getMergeJobs :: BotState -> Set PatchId
-getMergeJobs (BotState patches) = Map.keysSet $ Map.filter (== MergeJob) patches
+clearMergeQueue :: BotState -> BotState
+clearMergeQueue state = state{mergeQueue = Set.empty}
 
-{- Modifying state -}
+insertMergeQueue :: Id PullRequest -> BotState -> BotState
+insertMergeQueue pr state@BotState{..} = state{mergeQueue = Set.insert pr mergeQueue}
 
--- | Add the given patch to the merge queue.
-insertMergeQueue :: PatchId -> BotState -> BotState
-insertMergeQueue patch (BotState patches) = BotState $ Map.insert patch MergeQueue patches
-
--- | Remove the given patch from the merge queue.
-removeMergeQueue :: PatchId -> BotState -> BotState
-removeMergeQueue patch (BotState patches) = BotState $ Map.update checkQueue patch patches
-  where
-    checkQueue MergeQueue = Nothing
-    checkQueue patchState = Just patchState
-
--- | Clear the merge jobs, either after a successful merge or after cancelling the merge job.
-clearMergeJobs :: BotState -> BotState
-clearMergeJobs (BotState patches) = BotState $ Map.filter (/= MergeJob) patches
-
--- | Initialize a merge job with all the patches in the queue.
---
--- Assumes 'clearMergeJobs' has been run.
-initMergeJob :: BotState -> BotState
-initMergeJob (BotState patches) = BotState $ Map.map toMergeJob patches
-  where
-    toMergeJob MergeQueue = MergeJob
-    toMergeJob MergeJob = error "Found existing merge job in initMergeJob"
-    toMergeJob patchState = patchState
-
--- | Start a try job for the given patch.
-startTryJob :: PatchId -> BotState -> BotState
-startTryJob patch (BotState patches) = BotState $ Map.insert patch TryJob patches
+removeMergeQueue :: Id PullRequest -> BotState -> BotState
+removeMergeQueue pr state@BotState{..} = state{mergeQueue = Set.delete pr mergeQueue}
