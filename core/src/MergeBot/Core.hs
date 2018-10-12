9c803c87d4e752f2a4f4ca33d2f697ac9e4dfe9d
Brandon Chinn <brandon@leapyear.io>
2018-10-12 16:35:20 -0700

    Add stubs for core mergebot functionality

--- core/src/MergeBot/Core.hs
+++ core/src/MergeBot/Core.hs
@@ -8,7 +8,42 @@ Defines the core functionality of the merge bot.
 -}
 
 module MergeBot.Core
-  ( MonadGitHub(..)
+  ( listPullRequests
+  , getPullRequest
+  , tryPullRequest
+  , queuePullRequest
+  , unqueuePullRequest
+  , startMergeJob
+  , runMerge
   ) where
 
-import MergeBot.Core.Monad.Class
+import MergeBot.Core.Data
+import MergeBot.Core.State
+
+-- | List all open pull requests.
+listPullRequests :: Monad m => BotState -> m [PullRequest]
+listPullRequests = undefined
+
+-- | Return a single pull request.
+getPullRequest :: Monad m => PullRequestId -> m PullRequestDetail
+getPullRequest = undefined
+
+-- | Start a try job for the given pull request.
+tryPullRequest :: Monad m => PullRequestId -> m ()
+tryPullRequest = undefined
+
+-- | Queue the given pull request.
+queuePullRequest :: PullRequestId -> BotState -> BotState
+queuePullRequest = insertMergeQueue
+
+-- | Unqueue the given pull request.
+unqueuePullRequest :: PullRequestId -> BotState -> BotState
+unqueuePullRequest = removeMergeQueue
+
+-- | Start a merge job.
+startMergeJob :: Monad m => BotState -> m BotState
+startMergeJob = undefined
+
+-- | Merge pull requests after a successful merge job.
+runMerge :: Monad m => m ()
+runMerge = undefined
