7e8fb69a854a3da7f38362350635ffcd134f601e
Brandon Chinn <brandon@leapyear.io>
2018-10-14 10:43:19 -0700

    Move code to Data.GraphQL.Query.Schema

--- graphql/src/Data/GraphQL/Query.hs
+++ graphql/src/Data/GraphQL/Query.hs
@@ -6,44 +6,31 @@ Portability :  portable
 
 Definitions needed by GraphQL queries.
 -}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Data.GraphQL.Query
   ( Query
   , HasArgs(..)
   , readGraphQLFile
-  , getterFor
-  , Schema(..)
-  , GraphQLEnum(..)
   , object
-  , fromText
+  , module Query
   -- * Re-exports
   , Object
-  , Proxy(..)
-  , QuasiQuoter
   , (.=)
   ) where
 
 import Data.Aeson (Object, (.=))
 import Data.Aeson.Types (Pair)
 import qualified Data.HashMap.Strict as HashMap
-import Data.Proxy (Proxy(..))
-import Data.Text (Text)
-import qualified Data.Text as Text
+import Data.Proxy (Proxy)
 import Language.Haskell.TH
-import Language.Haskell.TH.Quote (QuasiQuoter(..))
 
 import Data.GraphQL.Query.Internal
+import Data.GraphQL.Query.Schema as Query
 
 -- | An alias for HashMap.fromList.
 object :: [Pair] -> Object
 object = HashMap.fromList
-
--- | An alias for Text.unpack.
-fromText :: Text -> String
-fromText = Text.unpack
 
 -- | A type class for GraphQL queries with arguments.
 class HasArgs r where
@@ -55,68 +42,3 @@ class HasArgs r where
 -- This function should go away when we generate the entire file with Template Haskell.
 readGraphQLFile :: FilePath -> ExpQ
 readGraphQLFile = undefined
-
--- | Return a QuasiQuoter that can parse the given schema.
---
--- For example, with the given result and schema:
---
--- @
--- {
---     "foo": {                  # SchemaObject
---          "a": 1,              #   SchemaInt
---          "nodes": [           #   SchemaList
---               { "b": true },  #     SchemaObject [("b", SchemaBool)]
---               { "b": false },
---               { "b": null },
---          ],
---          "c": "asdf",         #   SchemaMaybe SchemaString
---     }
--- }
--- @
---
--- The quasiquoter can be used in the following way:
---
--- @
--- get = getterFor schema -- the above schema
---
--- [get| foo.a |]         :: Result -> Int
--- [get| foo.nodes[] |]   :: Result -> [Object]
--- [get| foo.nodes[].b |] :: Result -> [Bool]
--- [get| foo.c |]         :: Result -> Maybe Text
--- [get| foo.c! |]        :: Result -> Text
--- @
---
--- Rules:
---
--- * 'x' is only valid if 'x' is Object, Int, Double, Text, Bool, or Maybe t.
---
---     * Returns the value of 'x'
---     * 'Maybe t' if 'x' is within an inline fragment
---     * if 'x!' on 'Maybe t', calls 'fromJust'
---
--- * 'x[]' is only valid if 'x' is [t]. Returns the value of 'x'
--- * 'x.y' is only valid if 'x' is an Object, [Object], or Maybe Object. Returns the value of the
---   key 'y' in the Object(s).
-getterFor :: Schema -> QuasiQuoter
-getterFor _ = QuasiQuoter
-  { quoteExp = undefined
-  , quotePat = \_ -> error "'get' can only used as an expression"
-  , quoteType = \_ -> error "'get' can only used as an expression"
-  , quoteDec = \_ -> error "'get' can only used as an expression"
-  }
-
--- | A datatype to represent the schema of a GraphQL result.
-data Schema
-  = SchemaBool
-  | SchemaInt
-  | SchemaDouble
-  | SchemaString
-  | SchemaScalar
-  | forall e. SchemaEnum (GraphQLEnum e => Proxy e)
-  | SchemaMaybe Schema
-  | SchemaList Schema
-  | SchemaObject [(Text, Schema)]
-
--- | A type class for resolving SchemaEnum.
-class GraphQLEnum e where
-  parseEnum :: Proxy e -> Text -> e
---
+++ graphql/src/Data/GraphQL/Query/Schema.hs
@@ -0,0 +1,95 @@
+{-|
+Module      :  Data.GraphQL.Query.Schema
+Maintainer  :  Brandon Chinn <brandon@leapyear.io>
+Stability   :  experimental
+Portability :  portable
+
+Definitions for defining and parsing schemas.
+-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Data.GraphQL.Query.Schema
+  ( getterFor
+  , Schema(..)
+  , GraphQLEnum(..)
+  , fromText
+  -- * Re-exports
+  , Proxy(..)
+  , QuasiQuoter
+  ) where
+
+import Data.Proxy (Proxy(..))
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Language.Haskell.TH.Quote (QuasiQuoter(..))
+
+-- | An alias for Text.unpack.
+fromText :: Text -> String
+fromText = Text.unpack
+
+-- | Return a QuasiQuoter that can parse the given schema.
+--
+-- For example, with the given result and schema:
+--
+-- @
+-- {
+--     "foo": {                  # SchemaObject
+--          "a": 1,              #   SchemaInt
+--          "nodes": [           #   SchemaList
+--               { "b": true },  #     SchemaObject [("b", SchemaBool)]
+--               { "b": false },
+--               { "b": null },
+--          ],
+--          "c": "asdf",         #   SchemaMaybe SchemaString
+--     }
+-- }
+-- @
+--
+-- If you have a variable named 'result' with the result of the GraphQL query, the quasiquoter can
+-- be used in the following way:
+--
+-- @
+-- get = getterFor schema -- the above schema
+--
+-- [get| result.foo.a |]         :: Int
+-- [get| result.foo.nodes[] |]   :: [Object]
+-- [get| result.foo.nodes[].b |] :: [Bool]
+-- [get| result.foo.c |]         :: Maybe Text
+-- [get| result.foo.c! |]        :: Text
+-- @
+--
+-- Rules:
+--
+-- * 'x' is only valid if 'x' is Object, Int, Double, Text, Bool, or Maybe t.
+--
+--     * Returns the value of 'x'
+--     * 'Maybe t' if 'x' is within an inline fragment
+--     * if 'x!' on 'Maybe t', calls 'fromJust'
+--
+-- * 'x[]' is only valid if 'x' is [t]. Returns the value of 'x'
+-- * 'x.y' is only valid if 'x' is an Object, [Object], or Maybe Object. Returns the value of the
+--   key 'y' in the Object(s).
+getterFor :: Schema -> QuasiQuoter
+getterFor _ = QuasiQuoter
+  { quoteExp = undefined
+  , quotePat = \_ -> error "'get' can only used as an expression"
+  , quoteType = \_ -> error "'get' can only used as an expression"
+  , quoteDec = \_ -> error "'get' can only used as an expression"
+  }
+
+-- | A datatype to represent the schema of a GraphQL result.
+data Schema
+  = SchemaBool
+  | SchemaInt
+  | SchemaDouble
+  | SchemaString
+  | SchemaScalar
+  | forall e. SchemaEnum (GraphQLEnum e => Proxy e)
+  | SchemaMaybe Schema
+  | SchemaList Schema
+  | SchemaObject [(Text, Schema)]
+
+-- | A type class for resolving SchemaEnum.
+class GraphQLEnum e where
+  parseEnum :: Proxy e -> Text -> e
