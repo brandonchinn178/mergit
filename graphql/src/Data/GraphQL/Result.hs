5ad517179ad0ae014049b989509ef8f439592af1
Brandon Chinn <brandon@leapyear.io>
2018-10-15 13:01:30 -0700

    Use megaparsec to parse quasiquoter

--- graphql/src/Data/GraphQL/Result.hs
+++ graphql/src/Data/GraphQL/Result.hs
@@ -8,6 +8,7 @@ Definitions for defining schemas and querying GraphQL results.
 -}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Data.GraphQL.Result
   ( Schema(..)
@@ -17,9 +18,15 @@ module Data.GraphQL.Result
   , QuasiQuoter
   ) where
 
+import Control.Applicative (many, (<|>))
+import Control.Monad (void)
 import Data.Proxy (Proxy(..))
 import Data.Text (Text)
+import Data.Void (Void)
+import Language.Haskell.TH
 import Language.Haskell.TH.Quote (QuasiQuoter(..))
+import Text.Megaparsec (Parsec, eof, parse, parseErrorPretty)
+import Text.Megaparsec.Char (alphaNumChar, letterChar, space, string)
 
 import Data.GraphQL.Result.Parse as Result
 
@@ -60,6 +67,7 @@ data Schema
 -- get = getterFor schema -- the above schema
 --
 -- [get| result.foo.a |]          :: Int
+-- [get| result.foo.nodes |]      :: [Object]
 -- [get| result.foo.nodes[] |]    :: [Object]
 -- [get| result.foo.nodes[].b |]  :: [Maybe Bool]
 -- [get| result.foo.nodes[].b! |] :: [Bool]
@@ -68,19 +76,61 @@ data Schema
 --
 -- Rules:
 --
--- * 'x' is only valid if 'x' is Object, Int, Double, Text, Bool, or Maybe t.
+-- * 'x' returns the value of 'x'
 --
 --     * Returns the value of 'x'
---     * 'Maybe t' if 'x' is within an inline fragment
---     * if 'x!' on 'Maybe t', calls 'fromJust'
+--     * Will be 'Maybe' if 'x' is within an inline fragment
 --
--- * 'x[]' is only valid if 'x' is [t]. Returns the value of 'x'
--- * 'x.y' is only valid if 'x' is an Object, [Object], or Maybe Object. Returns the value of the
---   key 'y' in the Object(s).
+-- * 'x.y' is only valid if 'x' is an 'Object' or a 'Maybe Object'. Returns the value of the key 'y'
+--   in the Object(s).
+--
+-- * 'x!' is only valid if 'x' is a 'Maybe'. Calls 'fromJust' on 'x'.
+--
+-- * 'x[]' is only valid if 'x' is [t]. Applies remaining rules as an fmap over the values of 'x'.
+--
+--     * 'x[]' without anything after is equivalent to 'x'
+--     * 'x[].y' gets the key 'y' in all the objects in 'x'
+--     * 'x[]!' calls `fromJust` on all values in 'x'
 getterFor :: Schema -> QuasiQuoter
 getterFor _ = QuasiQuoter
-  { quoteExp = undefined
+  { quoteExp = \s -> do
+      expr <- either (fail . parseErrorPretty) return $ parse getterExpr s s
+      error "TODO"
   , quotePat = \_ -> error "'get' can only used as an expression"
   , quoteType = \_ -> error "'get' can only used as an expression"
   , quoteDec = \_ -> error "'get' can only used as an expression"
   }
+
+{- Parser for getter quasiquotes -}
+
+data GetterExpr = GetterExpr
+  { var   :: String
+  , terms :: [GetterTerm]
+  } deriving (Show)
+
+data GetterTerm
+  = GetterKey String
+  | GetterBang
+  | GetterList
+  deriving (Show)
+
+type Parser = Parsec Void String
+
+identifier :: Parser String
+identifier = (:) <$> letterChar <*> many alphaNumChar
+
+getterExpr :: Parser GetterExpr
+getterExpr = do
+  space
+  var <- identifier
+  terms <- many getterTerm
+  space
+  void eof
+  return GetterExpr{..}
+
+getterTerm :: Parser GetterTerm
+getterTerm = parseGetterKey <|> parseGetterBang <|> parseGetterList
+  where
+    parseGetterKey = GetterKey <$> (string "." *> identifier)
+    parseGetterBang = string "!" *> pure GetterBang
+    parseGetterList = string "[]" *> pure GetterList
