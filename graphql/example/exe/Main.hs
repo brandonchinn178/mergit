e3663f251faf855b4e18ad17667844df64295504
Brandon Chinn <brandon@leapyear.io>
2018-10-17 14:51:42 -0700

    Showcase '>' operator in example, refactor tests

--- graphql/example/exe/Main.hs
+++ graphql/example/exe/Main.hs
@@ -1,8 +1,8 @@
 import qualified Data.Map.Strict as Map
-import Example (getCountries, runApp)
+import Example (getContinents, runApp)
 
 main :: IO ()
-main = mapM_ printContinent . Map.toList =<< runApp getCountries
+main = mapM_ printContinent . Map.toList =<< runApp getContinents
   where
     printContinent (continent, countries) = do
       putStrLn $ "Continent: " ++ show continent
--- graphql/example/test/Test.hs
+++ graphql/example/test/Test.hs
@@ -9,7 +9,7 @@ import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.Golden (goldenVsString)
 
 import qualified Countries
-import Example (App(..), getCountries)
+import Example (App(..), getContinents, getCountries)
 
 data MockData = MockData
   { mockCountries :: Value
@@ -29,50 +29,54 @@ goldens name = goldenVsString name fp . fmap (ByteString.pack . show)
   where
     fp = "test/goldens/" ++ name ++ ".golden"
 
+mock1 :: MockData
+mock1 = MockData
+  { mockCountries = [aesonQQ|
+      {
+        "countries": [
+          {
+            "name": "Foo",
+            "continent": {
+              "code": "EU"
+            }
+          }
+        ]
+      }
+    |]
+  }
+
+mock2 :: MockData
+mock2 = MockData
+  { mockCountries = [aesonQQ|
+      {
+        "countries": [
+          {
+            "name": "Bar",
+            "continent": {
+              "code": "NA"
+            }
+          },
+          {
+            "name": "Baz",
+            "continent": {
+              "code": "SA"
+            }
+          },
+          {
+            "name": "Quux",
+            "continent": {
+              "code": "SA"
+            }
+          }
+        ]
+      }
+    |]
+  }
+
 main :: IO ()
 main = defaultMain $ testGroup "graphql-example-test"
-  [ goldens "mock1" $
-      let mock = MockData
-            { mockCountries = [aesonQQ|
-                {
-                  "countries": [
-                    {
-                      "name": "Foo",
-                      "continent": {
-                        "code": "EU"
-                      }
-                    }
-                  ]
-                }
-              |]
-            }
-      in runMockApp mock getCountries
-  , goldens "mock2" $
-      let mock = MockData
-            { mockCountries = [aesonQQ|
-                {
-                  "countries": [
-                    {
-                      "name": "Bar",
-                      "continent": {
-                        "code": "NA"
-                      }
-                    },
-                    {
-                      "name": "Baz",
-                      "continent": {
-                        "code": "SA"
-                      }
-                    },
-                    {
-                      "name": "Quux",
-                      "continent": {
-                        "code": "SA"
-                      }
-                    }
-                  ]
-                }
-              |]
-            }
-      in runMockApp mock getCountries
+  [ goldens "mockContinents1" $ runMockApp mock1 getContinents
+  , goldens "mockContinents2" $ runMockApp mock2 getContinents
+  , goldens "mockCountries1" $ runMockApp mock1 getCountries
+  , goldens "mockCountries2" $ runMockApp mock2 getCountries
   ]
