f7ef0f6b500e63cb5edd075724b2999334194c5a
Brandon Chinn <brandon@leapyear.io>
2018-11-02 16:28:21 -0700

    Move pull request queries into MergeBot.Core.PullRequest

--- core/src/MergeBot/Core.hs
+++ core/src/MergeBot/Core.hs
@@ -10,8 +10,6 @@ Defines the core functionality of the merge bot.
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE RecordWildCards #-}
 
 module MergeBot.Core
   ( listPullRequests
@@ -23,111 +21,38 @@ module MergeBot.Core
   , runMerge
   ) where
 
-import Control.Monad (forM, forM_)
+import Control.Monad (forM_)
 import Control.Monad.Catch (MonadCatch)
 import Control.Monad.Reader (MonadReader, asks)
-import Data.GraphQL (MonadQuery, runQuery)
-import qualified Data.Map.Strict as Map
-import Data.Maybe (fromMaybe, isJust, isNothing)
+import Data.GraphQL (MonadQuery)
+import Data.Map.Strict ((!?))
+import Data.Maybe (fromMaybe)
 import qualified Data.Set as Set
-import Data.Text (Text)
 
 import MergeBot.Core.Branch
-import MergeBot.Core.CIStatus
 import MergeBot.Core.Data
 import MergeBot.Core.GitHub
-import qualified MergeBot.Core.GraphQL.PullRequest as PullRequest
-import qualified MergeBot.Core.GraphQL.PullRequestReview as PullRequestReview
-import MergeBot.Core.GraphQL.PullRequestReviewState (PullRequestReviewState(..))
-import qualified MergeBot.Core.GraphQL.PullRequests as PullRequests
-import qualified MergeBot.Core.GraphQL.PullRequestSimple as PullRequestSimple
-import MergeBot.Core.GraphQL.Scalars (parseUTCTime)
 import MergeBot.Core.Monad
+import MergeBot.Core.PullRequest
 import MergeBot.Core.State
 
 -- | List all open pull requests.
 listPullRequests :: (MonadReader BotEnv m, MonadQuery m) => BotState -> m [PullRequest]
 listPullRequests state = do
   branchStatuses <- getBranchStatuses state
-  (_repoOwner, _repoName) <- asks getRepo
-  queryAll $ \_after -> do
-    result <- runQuery PullRequests.query PullRequests.Args{..}
-    let info = [PullRequests.get| result.repository.pullRequests > info |]
-        prs = [PullRequests.get| @info.nodes![]! > pr |]
-        toPullRequest pr =
-          let prNum = [PullRequests.get| @pr.number |]
-          in PullRequest
-            { number  = prNum
-            , title   = [PullRequests.get| @pr.title |]
-            , author  = [PullRequests.get| @pr.author!.login |]
-            , created = parseUTCTime [PullRequests.get| @pr.createdAt |]
-            , updated = parseUTCTime [PullRequests.get| @pr.updatedAt |]
-            , status  = fromMaybe None $ Map.lookup prNum branchStatuses
-            }
-    return
-      ( map toPullRequest prs
-      , [PullRequests.get| @info.pageInfo.hasNextPage |]
-      , [PullRequests.get| @info.pageInfo.endCursor |]
-      )
+  getPullRequests $ fromMaybe None . (branchStatuses !?)
 
 -- | Return a single pull request.
 getPullRequest :: (MonadReader BotEnv m, MonadQuery m)
   => BotState -> PullRequestId -> m PullRequestDetail
-getPullRequest state _number = do
-  (_repoOwner, _repoName) <- asks getRepo
-
-  result <- runQuery PullRequest.query PullRequest.Args{..}
-  let pr = [PullRequest.get| result.repository.pullRequest! > pr |]
-      base = [PullRequest.get| @pr.baseRefName |]
-
-  reviews <- fmap resolveReviews $ queryAll $ \_after -> getReviews PullRequestReview.Args{..}
-  tryStatus <- getTryStatus _number
-  queue <- if _number `Set.member` mergeQueue
-    then fmap Just $ forM (Set.toList mergeQueue) $ \_number ->
-      getSimplePullRequest PullRequestSimple.Args{..}
-    else return Nothing
-  let mergeRun = Nothing -- TODO: get PRs in staging branch and the status of the merge run
-
-  return PullRequestDetail
-    { number      = [PullRequest.get| @pr.number |]
-    , title       = [PullRequest.get| @pr.title |]
-    , author      = [PullRequest.get| @pr.author!.login |]
-    , created     = parseUTCTime [PullRequest.get| @pr.createdAt |]
-    , updated     = parseUTCTime [PullRequest.get| @pr.updatedAt |]
-    , url         = [PullRequest.get| @pr.url |]
-    , body        = [PullRequest.get| @pr.bodyHTML |]
-    , commit      = [PullRequest.get| @pr.headRefOid |]
-    , base        = base
-    , approved    = not (null reviews) && all (== APPROVED) reviews
-    , tryRun      = TryRun <$> tryStatus
-    , mergeQueue  = queue
-    , mergeRun    = mergeRun
-    , canTry      = isNothing mergeRun && maybe True (not . isPending) tryStatus
-    , canQueue    = isNothing queue && isNothing mergeRun && base == "master"
-    , canUnqueue  = isJust queue
-    }
+getPullRequest state prNum = do
+  tryStatus <- getTryStatus prNum
+  getPullRequestDetail prNum tryStatus maybeQueue
   where
-    mergeQueue = getMergeQueue state
-    getReviews args = do
-      result <- runQuery PullRequestReview.query args
-      let info = [PullRequestReview.get| result.repository.pullRequest!.reviews! > info |]
-          reviews = [PullRequestReview.get| @info.nodes![]! > review |]
-          fromReview review =
-            ( [PullRequestReview.get| @review.author!.login |]
-            , [PullRequestReview.get| @review.state |]
-            )
-      return
-        ( map fromReview reviews
-        , [PullRequestReview.get| @info.pageInfo.hasNextPage |]
-        , [PullRequestReview.get| @info.pageInfo.endCursor |]
-        )
-    getSimplePullRequest args = do
-      result <- runQuery PullRequestSimple.query args
-      let pr = [PullRequestSimple.get| result.repository.pullRequest! > pr |]
-      return PullRequestSimple
-        { number = [PullRequestSimple.get| @pr.number |]
-        , title  = [PullRequestSimple.get| @pr.title |]
-        }
+    queue = getMergeQueue state
+    maybeQueue = if prNum `Set.member` queue
+      then Just $ Set.toList queue
+      else Nothing
 
 -- | Start a try job for the given pull request.
 tryPullRequest :: (MonadCatch m, MonadGitHub m, MonadReader BotEnv m, MonadQuery m)
@@ -159,24 +84,5 @@ runMerge = do
     -- TODO: handle master being different than when staging started
     Nothing -> fail "Update was not a fast-forward"
     Just prs -> forM_ prs $ \prNum -> do
-      result <- runQuery PullRequest.query PullRequest.Args{_number = prNum, ..}
-      let pr = [PullRequest.get| result.repository.pullRequest! > pr |]
-          branch = [PullRequest.get| @pr.headRefName |]
-      deleteBranch branch
+      deleteBranch =<< getBranch prNum
       deleteBranch $ toTryBranch prNum
-
-{- Helpers -}
-
--- | Resolve the given pull request reviews according to the given rules:
---
--- * Only track APPROVED or CHANGES_REQUESTED reviews
--- * A given author's reviews later in the list overrule their reviews earlier in the list
-resolveReviews :: [(Text, PullRequestReviewState)] -> [PullRequestReviewState]
-resolveReviews = squashReviews' Map.empty
-  where
-    squashReviews' reviews [] = Map.elems reviews
-    squashReviews' reviews ((author, review):rest) =
-      let reviews' = if review `elem` [APPROVED, CHANGES_REQUESTED]
-            then Map.insert author review reviews
-            else reviews
-      in squashReviews' reviews' rest
---
+++ core/src/MergeBot/Core/PullRequest.hs
@@ -0,0 +1,160 @@
+{-|
+Module      :  MergeBot.Core.PullRequest
+Maintainer  :  Brandon Chinn <brandon@leapyear.io>
+Stability   :  experimental
+Portability :  portable
+
+Defines functions to query and manage pull requests.
+-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module MergeBot.Core.PullRequest
+  ( getPullRequests
+  , getPullRequestDetail
+  , getPullRequestSimple
+  , getBranch
+  ) where
+
+import Control.Monad.Reader (MonadReader, asks)
+import Data.Aeson (Object)
+import Data.GraphQL (MonadQuery, runQuery)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (isJust, isNothing)
+import Data.Text (Text)
+
+import MergeBot.Core.CIStatus (isPending)
+import MergeBot.Core.Data
+    ( BotStatus
+    , CIStatus(..)
+    , PullRequest(..)
+    , PullRequestDetail(..)
+    , PullRequestId
+    , PullRequestSimple(..)
+    , TryRun(..)
+    )
+import MergeBot.Core.GitHub (queryAll)
+import qualified MergeBot.Core.GraphQL.PullRequest as PullRequest
+import qualified MergeBot.Core.GraphQL.PullRequestReview as PullRequestReview
+import MergeBot.Core.GraphQL.PullRequestReviewState (PullRequestReviewState(..))
+import qualified MergeBot.Core.GraphQL.PullRequests as PullRequests
+import MergeBot.Core.GraphQL.Scalars (parseUTCTime)
+import MergeBot.Core.Monad (BotEnv, getRepo)
+
+-- | Get all open pull requests.
+getPullRequests :: (MonadReader BotEnv m, MonadQuery m) =>
+  (PullRequestId -> BotStatus) -> m [PullRequest]
+getPullRequests getStatus = do
+  (_repoOwner, _repoName) <- asks getRepo
+  queryAll $ \_after -> do
+    result <- runQuery PullRequests.query PullRequests.Args{..}
+    let info = [PullRequests.get| result.repository.pullRequests > info |]
+        prs = [PullRequests.get| @info.nodes![]! > pr |]
+        toPullRequest pr =
+          let prNum = [PullRequests.get| @pr.number |]
+          in PullRequest
+            { number  = prNum
+            , title   = [PullRequests.get| @pr.title |]
+            , author  = [PullRequests.get| @pr.author!.login |]
+            , created = parseUTCTime [PullRequests.get| @pr.createdAt |]
+            , updated = parseUTCTime [PullRequests.get| @pr.updatedAt |]
+            , status  = getStatus prNum
+            }
+    return
+      ( map toPullRequest prs
+      , [PullRequests.get| @info.pageInfo.hasNextPage |]
+      , [PullRequests.get| @info.pageInfo.endCursor |]
+      )
+
+-- | Get the GraphQL result for a single pull request.
+getPullRequest :: (MonadReader BotEnv m, MonadQuery m) => PullRequestId -> m Object
+getPullRequest prNum = do
+  (_repoOwner, _repoName) <- asks getRepo
+  result <- runQuery PullRequest.query PullRequest.Args{_number=prNum, ..}
+  return [PullRequest.get| result.repository.pullRequest! > pr |]
+
+-- | Get a simple pull request.
+getPullRequestSimple :: (MonadReader BotEnv m, MonadQuery m)
+  => PullRequestId -> m PullRequestSimple
+getPullRequestSimple prNum = do
+  pr <- getPullRequest prNum
+  return PullRequestSimple
+    { number = [PullRequest.get| @pr.number |]
+    , title  = [PullRequest.get| @pr.title  |]
+    }
+
+-- | Get the branch name for the pull request.
+getBranch :: (MonadReader BotEnv m, MonadQuery m)
+  => PullRequestId -> m Text
+getBranch prNum = do
+  pr <- getPullRequest prNum
+  return [PullRequest.get| @pr.headRefName |]
+
+-- | Get a detailed pull request.
+getPullRequestDetail :: (MonadReader BotEnv m, MonadQuery m)
+  => PullRequestId -> Maybe CIStatus -> Maybe [PullRequestId] -> m PullRequestDetail
+getPullRequestDetail prNum tryStatus maybeQueue = do
+  (_repoOwner, _repoName) <- asks getRepo
+
+  pr <- getPullRequest prNum
+  let base = [PullRequest.get| @pr.baseRefName |]
+
+  reviews <- getReviews prNum
+  queue <- traverse (mapM getPullRequestSimple) maybeQueue
+  let mergeRun = Nothing -- TODO: get PRs in staging branch and the status of the merge run
+
+  return PullRequestDetail
+    { number      = [PullRequest.get| @pr.number |]
+    , title       = [PullRequest.get| @pr.title |]
+    , author      = [PullRequest.get| @pr.author!.login |]
+    , created     = parseUTCTime [PullRequest.get| @pr.createdAt |]
+    , updated     = parseUTCTime [PullRequest.get| @pr.updatedAt |]
+    , url         = [PullRequest.get| @pr.url |]
+    , body        = [PullRequest.get| @pr.bodyHTML |]
+    , commit      = [PullRequest.get| @pr.headRefOid |]
+    , base        = base
+    , approved    = not (null reviews) && all (== APPROVED) reviews
+    , tryRun      = TryRun <$> tryStatus
+    , mergeQueue  = queue
+    , mergeRun    = mergeRun
+    , canTry      = isNothing mergeRun && maybe True (not . isPending) tryStatus
+    , canQueue    = isNothing queue && isNothing mergeRun && base == "master"
+    , canUnqueue  = isJust queue
+    }
+
+-- | Get the reviews for the given pull request.
+getReviews :: (MonadReader BotEnv m, MonadQuery m) => PullRequestId -> m [PullRequestReviewState]
+getReviews prNum = do
+  (_repoOwner, _repoName) <- asks getRepo
+  fmap resolveReviews $ queryAll $ \_after -> do
+    result <- runQuery PullRequestReview.query PullRequestReview.Args{_number=prNum,..}
+    let info = [PullRequestReview.get| result.repository.pullRequest!.reviews! > info |]
+        reviews = [PullRequestReview.get| @info.nodes![]! > review |]
+        fromReview review =
+          ( [PullRequestReview.get| @review.author!.login |]
+          , [PullRequestReview.get| @review.state |]
+          )
+    return
+      ( map fromReview reviews
+      , [PullRequestReview.get| @info.pageInfo.hasNextPage |]
+      , [PullRequestReview.get| @info.pageInfo.endCursor |]
+      )
+
+{- Helpers -}
+
+-- | Resolve the given pull request reviews according to the given rules:
+--
+-- * Only track APPROVED or CHANGES_REQUESTED reviews
+-- * A given author's reviews later in the list overrule their reviews earlier in the list
+resolveReviews :: [(Text, PullRequestReviewState)] -> [PullRequestReviewState]
+resolveReviews = squashReviews' Map.empty
+  where
+    squashReviews' reviews [] = Map.elems reviews
+    squashReviews' reviews ((author, review):rest) =
+      let reviews' = if review `elem` [APPROVED, CHANGES_REQUESTED]
+            then Map.insert author review reviews
+            else reviews
+      in squashReviews' reviews' rest
