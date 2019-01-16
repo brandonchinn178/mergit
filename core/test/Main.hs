8c10d007542712c43a660f2c5fa20943369338cd
Brandon Chinn <brandon@leapyear.io>
2019-01-16 11:40:23 -0800

    Add BranchStatuses tests

--- core/test/Main.hs
+++ core/test/Main.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Test.Tasty
 
 import MergeBot.Core (getSessionInfo)
 import qualified MergeBot.Core.Branch as Branch
+import MergeBot.Core.Config (BranchConfig(..))
+import qualified MergeBot.Core.GraphQL.Enums.StatusState as StatusState
 import MergeBot.Core.Monad (runBot)
 import MergeBot.Core.Test
 
@@ -13,10 +17,44 @@ main = defaultMain $ testGroup "merge-bot-core-test"
 
 getBranchStatusesTests :: TestTree
 getBranchStatusesTests = testGroup "MergeBot.Core.Branch.getBranchStatuses"
-  [ goldens "branch_statuses_try" $ runTestApp (Branch.getBranchStatuses []) mockData
-      { mockBranches = [tryBranch 1]
-      }
+  [ testBranchStatuses "branch_statuses_try" [] [tryBranch]
+  , testBranchStatuses "branch_statuses_try_config" []
+      [ tryBranch
+          { mergeConfig = Just $ BranchConfig ["test1"]
+          }
+      ]
+  , testBranchStatuses "branch_statuses_try_no_config" []
+      [ tryBranch
+          { contexts = [("test1", StatusState.SUCCESS)]
+          }
+      ]
+  , testBranchStatuses "branch_statuses_try_config_other" []
+      [ tryBranch
+          { mergeConfig = Just $ BranchConfig ["test1"]
+          , contexts = [("other", StatusState.SUCCESS)]
+          }
+      ]
+  , testBranchStatuses "branch_statuses_try_config_pending" []
+      [ tryBranch
+          { mergeConfig = Just $ BranchConfig ["test1"]
+          , contexts = [("test1", StatusState.PENDING)]
+          }
+      ]
+  , testBranchStatuses "branch_statuses_try_config_failure" []
+      [ tryBranch
+          { mergeConfig = Just $ BranchConfig ["test1"]
+          , contexts = [("test1", StatusState.FAILURE)]
+          }
+      ]
+  , testBranchStatuses "branch_statuses_try_config_success" []
+      [ tryBranch
+          { mergeConfig = Just $ BranchConfig ["test1"]
+          , contexts = [("test1", StatusState.SUCCESS)]
+          }
+      ]
   ]
   where
-    tryBranch :: Int -> Branch
-    tryBranch pr = baseBranch { branchName = "trying-" ++ show pr }
+    testBranchStatuses name queue branches = goldens name $
+      runTestApp (Branch.getBranchStatuses queue) mockData { mockBranches = branches }
+    tryName pr = "trying-" ++ show (pr :: Int)
+    tryBranch = baseBranch { branchName = tryName 1 }
