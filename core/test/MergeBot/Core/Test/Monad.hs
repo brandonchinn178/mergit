ba60f7d759f86af5ec033b2b8e8ae3c62f5492e3
Brandon Chinn <brandon@leapyear.io>
2019-01-21 16:54:14 -0800

    Remove BotAppT from TestApp stack, using StateT with MockData

--- core/test/MergeBot/Core/Test/Monad.hs
+++ core/test/MergeBot/Core/Test/Monad.hs
@@ -1,37 +1,52 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module MergeBot.Core.Test.Monad where
 
 import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Reader (MonadReader, runReaderT)
+import Control.Monad.Reader (MonadReader(..))
+import Control.Monad.State.Lazy (MonadState, StateT, evalStateT, gets)
 import Data.GraphQL
+    ( MonadQuery(..)
+    , QueryT
+    , mockedQuerySettings
+    , runQuerySafeMocked
+    , runQueryT
+    )
+import Data.GraphQL.TestUtils (mockWith)
 
 import MergeBot.Core.GraphQL.API (API)
-import MergeBot.Core.Monad (BotAppT(..), BotEnv(..))
+import MergeBot.Core.Monad (BotEnv(..))
 import MergeBot.Core.Test.Data (MockData)
 
 -- | The monad to run tests
-newtype TestApp a = TestApp (BotAppT IO a)
+newtype TestApp a = TestApp { unTestApp :: StateT MockData (QueryT API IO) a }
   deriving
     ( Functor
     , Applicative
     , Monad
     , MonadIO
-    , MonadReader BotEnv
-    , MonadQuery API
+    , MonadState MockData
     )
 
+instance MonadReader BotEnv TestApp where
+  ask = pure $ BotEnv
+    { repoOwner = "LeapYear"
+    , repoName = "merge-bot-test"
+    , ghToken = ""
+    , ghManager = error "Tests do not use an HTTP manager"
+    }
+  local _ = id
+
+instance MonadQuery API TestApp where
+  runQuerySafe query args = runQuerySafeMocked query args =<< gets mockWith
+
 runTestApp :: TestApp a -> MockData -> IO a
-runTestApp (TestApp botApp) mock =
-  runQueryT (mockedQuerySettings mock) $ runReaderT (unBotApp botApp) botEnv
-  where
-    botEnv = BotEnv
-      { repoOwner = "LeapYear"
-      , repoName = "merge-bot-test"
-      , ghToken = ""
-      , ghManager = error "Tests do not use an HTTP manager"
-      }
+runTestApp app mockData =
+  runQueryT (mockedQuerySettings mockData)
+  . (`evalStateT` mockData)
+  . unTestApp
+  $ app
