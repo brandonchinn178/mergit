0300470c9a5ec8e71642cb3e134cb58220f718aa
Brandon Chinn <brandon@leapyear.io>
2019-01-29 14:19:21 -0800

    Upgrade LTS 13.5 (#40)
    * Upgrade LTS 13.5
    
    * Separate building memory-intensive deps

--- scripts/install-stack-deps.sh
+++ scripts/install-stack-deps.sh
@@ -4,5 +4,8 @@
 
 set -eo pipefail
 
+# Dependencies that take a lot of memory that shouldn't run in parallel
+stack build Cabal
+
 stack build --test --only-dependencies
 stack build hlint stylish-haskell
