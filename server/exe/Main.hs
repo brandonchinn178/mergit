09b31fdd74326068e109c62f0ef6250482f080ad
Brandon Chinn <brandon@leapyear.io>
2019-02-04 11:19:04 -0800

    Add polling CI for merging PRs (revert in v1)

--- server/exe/Main.hs
+++ server/exe/Main.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE DataKinds #-}
+
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forM_, void, when)
+import Control.Monad.IO.Class (liftIO)
+import Data.Maybe (fromJust)
 import Network.Wai.Handler.Warp (run)
 
+import MergeBot.Core (runMerge)
+import MergeBot.Core.Branch (getStagingBranches, getStagingStatus)
+import MergeBot.Core.CIStatus (isSuccess)
+import MergeBot.Core.Monad (runBot)
 import MergeBot.Server (initApp)
+import MergeBot.Server.Monad (MergeBotEnv(..), initEnv)
 
 main :: IO ()
-main = run 3000 =<< initApp
+main = do
+  pollCI
+  run 3000 =<< initApp
+
+-- | Poll CI to check if any staging branches should be merged.
+--
+-- This function should go away when using GitHub webhooks to detect when to merge PRs.
+pollCI :: IO ()
+pollCI = void $ forkIO $ do
+  config <- botConfig <$> initEnv
+  runBot config go
+  where
+    tenMinutesInMicroSecs = 600000000
+    go = do
+      baseBranches <- getStagingBranches
+      forM_ baseBranches $ \baseBranch -> do
+        ciStatus <- fromJust <$> getStagingStatus baseBranch
+        when (isSuccess ciStatus) $ runMerge baseBranch
+
+      liftIO $ threadDelay tenMinutesInMicroSecs
+      go
