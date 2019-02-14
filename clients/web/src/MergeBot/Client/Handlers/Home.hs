44ebf58f9d8fe57bf92302ed2d43f01764c63d02
Brandon Chinn <brandon@leapyear.io>
2019-02-13 16:00:36 -0800

    Pretty print times

--- clients/web/src/MergeBot/Client/Handlers/Home.hs
+++ clients/web/src/MergeBot/Client/Handlers/Home.hs
@@ -6,16 +6,18 @@ module MergeBot.Client.Handlers.Home
   ( getHomeR
   ) where
 
+import Control.Monad.IO.Class (liftIO)
 import Network.HTTP.Types (StdMethod(..))
 import Yesod
 
 import MergeBot.Client.App (Handler, callAPI)
-import MergeBot.Client.Utils (widgetFile)
+import MergeBot.Client.Utils (mkPrettyTime, widgetFile)
 import MergeBot.Core.Data (PullRequest(..))
 
 getHomeR :: Handler Html
 getHomeR = do
   prs <- callAPI @[PullRequest] GET "/pulls/"
+  prettyTime <- liftIO mkPrettyTime
   defaultLayout $ do
     setTitle "Home"
     $(widgetFile "home")
--- clients/web/src/MergeBot/Client/Utils.hs
+++ clients/web/src/MergeBot/Client/Utils.hs
@@ -1,8 +1,10 @@
 module MergeBot.Client.Utils
   ( widgetFile
+  , mkPrettyTime
   ) where
 
 import Data.Default (def)
+import Data.Time (UTCTime, diffUTCTime, getCurrentTime)
 import Language.Haskell.TH.Syntax (Exp, Q)
 import Yesod.Default.Util (widgetFileNoReload, widgetFileReload)
 
@@ -16,3 +18,28 @@ widgetFile = widgetFile' def
     widgetFile' = if appReloadTemplates
       then widgetFileReload
       else widgetFileNoReload
+
+{- Rendering functions -}
+
+-- | Return a function that can convert a UTCTime into human-readable durations from the current
+-- time; e.g. '2h ago'.
+mkPrettyTime :: IO (UTCTime -> String)
+mkPrettyTime = prettyTime <$> getCurrentTime
+  where
+    prettyTime now time = showDuration (now `diffUTCTime` time) ++ " ago"
+    showDuration diff =
+      let loop _ [] = error "mkPrettyTime: unreachable"
+          loop x ((base, unit):rest) =
+            case x `divMod` base of
+              (0, r) -> show r ++ unit
+              (q, _) -> loop q rest
+      in loop (round diff) units
+    units =
+      [ (60, "s")
+      , (60, "m")
+      , (24, "h")
+      , (7, "d")
+      , (52, "w")
+      , (inf, "y")
+      ]
+    inf = round (read "Infinity" :: Double) :: Integer
