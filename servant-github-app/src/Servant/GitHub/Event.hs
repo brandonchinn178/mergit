1eae8b837bdc6df885540ee1f0b655213856a9bc
Brandon Chinn <brandon@leapyear.io>
2019-03-01 10:42:50 -0800

    Add CheckRunEvent

--- servant-github-app/src/Servant/GitHub/Event.hs
+++ servant-github-app/src/Servant/GitHub/Event.hs
@@ -6,16 +6,20 @@ Portability :  portable
 
 Defines all the possible GitHub events that can be sent.
 -}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Servant.GitHub.Event
   ( GitHubEventType(..)
-  , FromEventType
-  , eventName
+  , IsGitHubEvent(..)
+  , module EventSchemas
   ) where
+
+import Data.Aeson.Schema (SchemaType)
+
+import Servant.GitHub.Event.CheckRun as EventSchemas
 
 data GitHubEventType
   = CheckRunEvent
@@ -64,51 +68,10 @@ data GitHubEventType
   | WatchEvent
   deriving (Show)
 
-eventName :: GitHubEventType -> String
-eventName = \case
-  CheckRunEvent -> "check_run"
-  CheckSuiteEvent -> "check_suite"
-  CommitCommentEvent -> "commit_comment"
-  ContentReferenceEvent -> "content_reference"
-  CreateEvent -> "create"
-  DeleteEvent -> "delete"
-  DeploymentEvent -> "deployment"
-  DeploymentStatusEvent -> "deployment_status"
-  DownloadEvent -> "download"
-  FollowEvent -> "follow"
-  ForkEvent -> "fork"
-  ForkApplyEvent -> "fork_apply"
-  GitHubAppAuthorizationEvent -> "github_app_authorization"
-  GistEvent -> "gist"
-  GollumEvent -> "gollum"
-  InstallationEvent -> "installation"
-  InstallationRepositoriesEvent -> "installation_repositories"
-  IssueCommentEvent -> "issue_comment"
-  IssuesEvent -> "issues"
-  LabelEvent -> "label"
-  MarketplacePurchaseEvent -> "marketplace_purchase"
-  MemberEvent -> "member"
-  MembershipEvent -> "membership"
-  MilestoneEvent -> "milestone"
-  OrganizationEvent -> "organization"
-  OrgBlockEvent -> "org_block"
-  PageBuildEvent -> "page_build"
-  ProjectCardEvent -> "project_card"
-  ProjectColumnEvent -> "project_column"
-  ProjectEvent -> "project"
-  PublicEvent -> "public"
-  PullRequestEvent -> "pull_request"
-  PullRequestReviewEvent -> "pull_request_review"
-  PullRequestReviewCommentEvent -> "pull_request_review_comment"
-  PushEvent -> "push"
-  ReleaseEvent -> "release"
-  RepositoryEvent -> "repository"
-  RepositoryImportEvent -> "repository_import"
-  RepositoryVulnerabilityAlertEvent -> "repository_vulnerability_alert"
-  SecurityAdvisoryEvent -> "security_advisory"
-  StatusEvent -> "status"
-  TeamEvent -> "team"
-  TeamAddEvent -> "team_add"
-  WatchEvent -> "watch"
+class IsGitHubEvent (e :: GitHubEventType) where
+  type EventSchema e :: SchemaType
+  eventName :: String
 
-type family FromEventType (e :: GitHubEventType) :: *
+instance IsGitHubEvent 'CheckRunEvent where
+  type EventSchema 'CheckRunEvent = CheckRunSchema
+  eventName = "check_run"
