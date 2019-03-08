e13dde28cf23d164ec6c33d61c5c01b3be6164a9
Brandon Chinn <brandon@leapyear.io>
2019-03-08 11:27:59 -0800

    Test template-haskell-test-utils

--- template-haskell-test-utils/test/Main.hs
+++ template-haskell-test-utils/test/Main.hs
@@ -1,2 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Data.Void (Void)
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Language.Haskell.TH.TestUtils
+import TH
+
 main :: IO ()
-main = putStrLn "Hello world"
+main = defaultMain $ testGroup "template-haskell-test-utils"
+  [ testCase "Maybe" $
+    $(tryQ' $ firstConstrForType "Maybe") @== Right "Nothing"
+  , testCase "NonExistent" $
+    $(tryQ' $ firstConstrForType "NonExistent") @== Left "Type does not exist: NonExistent"
+  , testCase "Show" $
+    $(tryQ' $ firstConstrForType "Show") @== Left "Not a data type: Show"
+  , testCase "Void" $
+    $(tryQ' $ firstConstrForType "Void") @== Left "Data type has no constructors: Void"
+  ]
+
+-- | Helper to specify the type of the splice.
+(@==) :: Either String String -> Either String String -> IO ()
+(@==) = (@?=)
