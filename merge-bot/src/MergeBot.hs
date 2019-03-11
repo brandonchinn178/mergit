3604b69405016c9ef0bcae50d81d3814621c6ee0
Brandon Chinn <brandon@leapyear.io>
2019-03-11 10:28:45 -0700

    Add MergeBot package

---
+++ merge-bot/src/MergeBot.hs
@@ -0,0 +1,10 @@
+{-|
+Module      :  MergeBot
+Maintainer  :  Brandon Chinn <brandon@leapyear.io>
+Stability   :  experimental
+Portability :  portable
+
+This module defines the entrypoint for the MergeBot GitHub application.
+-}
+
+module MergeBot where
