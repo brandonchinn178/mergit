3783c14a0378057a739f9c2ddf874f201663b795
Brandon Chinn <brandon@leapyear.io>
2019-03-12 17:15:43 -0700

    Add BranchTree GraphQL query

---
+++ merge-bot-core/src/MergeBot/Core/GraphQL/Custom/GitObjectID.hs
@@ -0,0 +1,17 @@
+{-|
+Module      :  MergeBot.Core.GraphQL.Custom.GitObjectID
+Maintainer  :  Brandon Chinn <brandon@leapyear.io>
+Stability   :  experimental
+Portability :  portable
+
+Defines the GitObjectID data type.
+-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module MergeBot.Core.GraphQL.Custom.GitObjectID where
+
+import Data.Aeson (FromJSON)
+import Data.Text (Text)
+
+newtype GitObjectID = GitObjectID { unOID :: Text }
+  deriving (Show,FromJSON)
