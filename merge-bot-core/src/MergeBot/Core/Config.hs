ff97e399f428ff43d2e40ee88801601f5c827b55
Brandon Chinn <brandon@leapyear.io>
2019-03-18 11:24:51 -0700

    Add .lymerge.yaml configuration type

---
+++ merge-bot-core/src/MergeBot/Core/Config.hs
@@ -0,0 +1,35 @@
+{-|
+Module      :  MergeBot.Core.Config
+Maintainer  :  Brandon Chinn <brandon@leapyear.io>
+Stability   :  experimental
+Portability :  portable
+
+Defines the data type representing the ".lymerge.yaml" configuration file.
+-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module MergeBot.Core.Config
+  ( BotConfig(..)
+  , configFileName
+  ) where
+
+import Data.Aeson (FromJSON(..), ToJSON(..), object, withObject, (.:), (.=))
+import Data.Text (Text)
+
+data BotConfig = BotConfig
+  { requiredStatuses :: [Text]
+  } deriving (Show)
+
+instance ToJSON BotConfig where
+  toJSON BotConfig{..} = object
+    [ "statuses" .= requiredStatuses
+    ]
+
+instance FromJSON BotConfig where
+  parseJSON = withObject "BotConfig" $ \o ->
+    BotConfig
+      <$> o .: "statuses"
+
+configFileName :: Text
+configFileName = ".lymerge.yaml"
