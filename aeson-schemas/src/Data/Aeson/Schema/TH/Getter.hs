81c296f72eeb5e6968c8b2613e571a857801d819
Brandon Chinn <brandon@leapyear.io>
2019-03-21 15:20:32 -0700

    Add mkGetter documentation

--- aeson-schemas/src/Data/Aeson/Schema/TH/Getter.hs
+++ aeson-schemas/src/Data/Aeson/Schema/TH/Getter.hs
@@ -20,7 +20,47 @@ import Data.Aeson.Schema.TH.Get (generateGetterExp)
 import Data.Aeson.Schema.TH.Parse (GetterExp(..), getterExp, parse)
 import Data.Aeson.Schema.TH.Utils (reifySchema, unwrapType)
 
--- | TODO
+-- | A helper that generates a 'get' expression and a type alias for the result of the expression.
+--
+-- > mkGetter "Node" "getNodes" ''MySchema ".nodes![]"
+-- >
+-- > -- generates roughly:
+-- > type Node = Object ('SchemaObject '[ '("b", 'SchemaMaybe 'SchemaBool) ])
+-- > getNodes :: Object MySchema -> [Node]
+-- > getNodes = [get| .nodes![] |]
+--
+-- 'mkGetter' takes four arguments:
+--
+--   [@unwrapName@] The name of the type synonym to store the unwrapped schema as
+--
+--   [@funcName@] The name of the getter function
+--
+--   [@startSchema@] The schema to extract/unwrap from
+--
+--   [@ops@] The operation to extract/unwrap from the schema
+--
+-- @ops@ is passed to the 'unwrap' and 'get' quasiquoters to create the unwrapped schema and the
+-- getter function. There is one subtlety that occurs from the use of the same @ops@ string for
+-- both the 'unwrap' and 'get' quasiquoters: 'unwrap' strips out intermediate functors, while 'get'
+-- applies within the functor. So in the above example, @".nodes![]"@ strips out the list when
+-- saving the schema to @Node@, while in the below example, @".nodes!"@ doesn't strip out the list
+-- when saving the schema to @Nodes@.
+--
+-- > mkGetter "Nodes" "getNodes" ''MySchema ".nodes!"
+-- >
+-- > -- generates roughly:
+-- > type Nodes = [Object ('SchemaObject '[ '("b", 'SchemaMaybe 'SchemaBool) ])]
+-- > getNodes :: Object MySchema -> Nodes
+-- > getNodes = [get| .nodes! |]
+--
+-- As another example,
+--
+-- > mkGetter "MyBool" "getMyBool" ''MySchema ".nodes?[].b"
+-- >
+-- > -- generates roughly:
+-- > type MyBool = Maybe Bool
+-- > getMyBool :: Object MySchema -> Maybe [MyBool]
+-- > getMyBool = [get| .nodes?[].b |]
 mkGetter :: String -> String -> Name -> String -> DecsQ
 mkGetter unwrapName funcName startSchemaName ops = do
   -- TODO: allow (Object schema)
