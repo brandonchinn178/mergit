46012eb49156f8f04fddeee2a1df2f72c927f3eb
Brandon Chinn <brandon@leapyear.io>
2019-04-02 16:22:44 -0700

    Add more logging

--- merge-bot/src/MergeBot/Handlers.hs
+++ merge-bot/src/MergeBot/Handlers.hs
@@ -20,8 +20,10 @@ module MergeBot.Handlers
   ) where
 
 import Control.Monad (unless, when)
+import Control.Monad.Logger (logInfoN)
 import Data.Aeson.Schema (Object, get)
 import qualified Data.Text as Text
+import GitHub.Data.GitObjectID (unOID)
 import qualified GitHub.Schema.Event.CheckRun as CheckRun
 import qualified GitHub.Schema.Event.CheckSuite as CheckSuite
 import qualified GitHub.Schema.Event.PullRequest as PullRequest
@@ -39,7 +41,9 @@ import MergeBot.Monad (runBotApp)
 handlePullRequest :: Object PullRequestEvent -> Token -> Handler ()
 handlePullRequest o = runBotApp repo $
   case [get| o.action |] of
-    PullRequest.OPENED -> Core.createCheckRuns [get| o.pull_request.head.sha |]
+    PullRequest.OPENED -> do
+      logInfoN $ "PR created: " <> Text.pack (show [get| o.pull_request.number |])
+      Core.createCheckRuns [get| o.pull_request.head.sha |]
     _ -> return ()
   where
     repo = [get| o.repository! |]
@@ -49,6 +53,7 @@ handleCheckSuite :: Object CheckSuiteEvent -> Token -> Handler ()
 handleCheckSuite o = runBotApp repo $
   case [get| o.action |] of
     CheckSuite.REQUESTED ->
+      -- create check runs for any commits pushed to a PR
       unless (null [get| o.check_suite.pull_requests |]) $
         Core.createCheckRuns [get| o.check_suite.head_sha |]
     _ -> return ()
@@ -65,19 +70,27 @@ handleCheckRun o = runBotApp repo $
         _ -> throwM $ NotOnePRInCheckRun o
 
       let prNum = [get| pr.number |]
+          prNum' = Text.pack $ show prNum
           sha = [get| o.check_run.head_sha |]
+          action = [get| o.requested_action!.identifier |]
 
-      unless (sha == [get| pr.head.sha |]) $ throwM $ CommitNotPRHead prNum sha
+      unless (sha == [get| pr.head.sha |]) $ do
+        logInfoN $ "Received action `" <> action <> "` for commit `" <> unOID sha <> "` on PR #" <> prNum'
+        throwM $ CommitNotPRHead prNum sha
 
-      case parseAction [get| o.requested_action!.identifier |] of
-        Just BotTry ->
-          Core.startTryJob
-            prNum
-            [get| pr.head.sha |]
-            [get| pr.base.ref |]
-        Just BotQueue -> Core.queuePR prNum
-        Just BotDequeue -> Core.dequeuePR prNum
-        Just BotResetMerge -> Core.resetMerge prNum
+      case parseAction action of
+        Just BotTry -> do
+          logInfoN $ "Trying PR #" <> prNum' <> " for commit: " <> unOID sha
+          Core.startTryJob prNum sha [get| pr.base.ref |]
+        Just BotQueue -> do
+          logInfoN $ "Queuing PR #" <> prNum'
+          Core.queuePR prNum
+        Just BotDequeue -> do
+          logInfoN $ "Dequeuing PR #" <> prNum'
+          Core.dequeuePR prNum
+        Just BotResetMerge -> do
+          logInfoN $ "Resetting merge check run for PR #" <> prNum'
+          Core.resetMerge prNum
         Nothing -> return ()
     _ -> return ()
   where
