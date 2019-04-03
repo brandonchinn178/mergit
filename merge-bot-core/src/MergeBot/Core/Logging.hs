b76821ba3b088a35b32760857429ef93193404d1
Brandon Chinn <brandon@leapyear.io>
2019-04-03 16:00:33 -0700

    Remove permissions check for logging

--- merge-bot-core/src/MergeBot/Core/Logging.hs
+++ merge-bot-core/src/MergeBot/Core/Logging.hs
@@ -16,11 +16,9 @@ import Control.Monad (unless)
 import Control.Monad.Logger (LogLevel(..), LoggingT, defaultLogStr, runLoggingT)
 import qualified Data.ByteString.Char8 as Char8
 import Data.Time (UTCTime, defaultTimeLocale, formatTime, getCurrentTime)
-import System.Directory (doesDirectoryExist)
 import System.FilePath ((</>))
 import System.IO (IOMode(..), stderr, withFile)
 import System.Log.FastLogger (fromLogStr)
-import UnliftIO.Exception (throwString)
 
 -- | A type alias for a basic logger running in IO.
 type LoggerIO = LoggingT IO
@@ -38,9 +36,6 @@ logDest date = "merge-bot__" <> date' <> ".log"
 -- | Run the given action, sending logs to 'getLogDest' and 'stderr'.
 runMergeBotLogging :: LoggingT m a -> m a
 runMergeBotLogging = flip runLoggingT $ \loc src lvl str -> do
-  exist <- doesDirectoryExist logDir
-  unless exist $ throwString $ "Log directory doesn't exist: " <> logDir
-
   now <- getCurrentTime
   let dest = logDir </> logDest now
       doLog h = Char8.hPutStr h $ fromLogStr $ defaultLogStr loc src lvl str
