b99b49f822e9049736bd7a995452a9e99556f0cb
Brandon Chinn <brandon@leapyear.io>
2019-04-09 10:56:10 -0700

    Run terraform fmt

--- deploy/main.tf
+++ deploy/main.tf
@@ -17,17 +17,19 @@ provider "aws" {
 }
 
 locals {
-  ami = "ami-011b3ccf1bd6db744" # RHEL 7.6 in us-east-1
+  ami           = "ami-011b3ccf1bd6db744" # RHEL 7.6 in us-east-1
   instance_type = "t2.micro"
-  ami_user = "ec2-user"
+  ami_user      = "ec2-user"
+
   tags = {
-    Name = "LY Merge Bot"
-    User = "MergeBot"
+    Name       = "LY Merge Bot"
+    User       = "MergeBot"
     Maintainer = "merge-bot"
   }
 
-  bot_conf_dir = "/etc/merge-bot.d/"
+  bot_conf_dir     = "/etc/merge-bot.d/"
   private_key_name = "github-app.pem"
+
   env_file_lines = [
     "export GITHUB_APP_ID=${var.app_id}",
     "export GITHUB_WEBHOOK_SECRET=${var.webhook_secret}",
@@ -50,23 +52,24 @@ module "keypair" {
 ## Resources ##
 
 resource "aws_security_group" "security_group" {
-  name = "merge_bot_security"
+  name        = "merge_bot_security"
   description = "Merge Bot security group"
-  vpc_id = "${data.aws_vpc.main.id}"
-  tags = "${local.tags}"
+  vpc_id      = "${data.aws_vpc.main.id}"
+  tags        = "${local.tags}"
 
   ingress {
     from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    to_port   = 22
+    protocol  = "tcp"
+
     # LeapYear Office
     cidr_blocks = ["96.82.102.113/32"]
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -79,30 +82,31 @@ resource "aws_security_group" "security_group" {
 }
 
 resource "aws_instance" "merge_bot" {
-  ami = "${local.ami}"
-  instance_type = "${local.instance_type}"
+  ami                    = "${local.ami}"
+  instance_type          = "${local.instance_type}"
   vpc_security_group_ids = ["${aws_security_group.security_group.id}"]
-  tags = "${local.tags}"
+  tags                   = "${local.tags}"
 
   key_name = "${module.keypair.id}"
+
   connection {
-    user = "${local.ami_user}"
+    user        = "${local.ami_user}"
     private_key = "${module.keypair.private_key_pem}"
   }
 
   provisioner "file" {
     destination = "~/merge-bot"
-    source = "${var.merge_bot}"
+    source      = "${var.merge_bot}"
   }
 
   provisioner "file" {
     destination = "~/${local.private_key_name}"
-    source = "${var.private_key}"
+    source      = "${var.private_key}"
   }
 
   provisioner "file" {
-    destination = "~/env",
-    content = "${join("\n", "${local.env_file_lines}")}"
+    destination = "~/env"
+    content     = "${join("\n", "${local.env_file_lines}")}"
   }
 
   provisioner "remote-exec" {
--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,6 +1,6 @@
 variable merge_bot {
   description = "The executable for running the merge bot."
-  default = "artifacts/merge-bot"
+  default     = "artifacts/merge-bot"
 }
 
 variable app_id {
@@ -13,10 +13,10 @@ variable webhook_secret {
 
 variable private_key {
   description = "The private key for the merge bot."
-  default = "artifacts/github-app.pem"
+  default     = "artifacts/github-app.pem"
 }
 
 variable user_agent {
   description = "The user agent to use when the merge bot talks with the GitHub API."
-  default = "LeapYear/merge-bot"
+  default     = "LeapYear/merge-bot"
 }
