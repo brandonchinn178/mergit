c1743938580e1df171f9329ab597f1e2eaab98d7
Brandon Chinn <brandon@leapyear.io>
2019-07-24 11:23:04 -0700

    Add BaseApp for all servant handlers

--- merge-bot/src/MergeBot/Monad.hs
+++ merge-bot/src/MergeBot/Monad.hs
@@ -7,7 +7,9 @@ Portability :  portable
 This module defines functions for running GitHubT actions.
 -}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
@@ -15,16 +17,22 @@ This module defines functions for running GitHubT actions.
 {-# LANGUAGE TypeApplications #-}
 
 module MergeBot.Monad
-  ( BotApp
+  ( BaseApp
+  , BaseServer
+  , runBaseApp
+  , runBaseHandler
+  , getGitHubAppParams
+  , getAuthParams
+    -- * BotAppT helpers
+  , BotApp
   , runBotApp
   , runBotAppForAllInstalls
-    -- * Helpers
-  , runIO
   ) where
 
 import Control.Monad (forM)
 import Control.Monad.Except (MonadError(..))
 import Control.Monad.IO.Class (MonadIO(..))
+import Control.Monad.Reader (ReaderT, asks, runReaderT)
 import Data.Aeson.Schema (Object, get, schema)
 import qualified Data.ByteString.Lazy.Char8 as Char8
 import Data.Maybe (fromMaybe)
@@ -34,50 +42,87 @@ import GitHub.REST
     (GHEndpoint(..), GitHubState(..), MonadGitHubREST(..), Token, runGitHubT)
 import GitHub.REST.Auth (getJWTToken)
 import Network.HTTP.Types (StdMethod(..))
-import Servant (Handler, ServantErr(..), err500)
-import Servant.GitHub (GitHubAppParams(..), loadGitHubAppParams)
+import Servant (Handler, ServantErr(..), ServerT, err500)
+import Servant.GitHub (GitHubAppParams(..))
 import Servant.GitHub.Security (getToken)
-import UnliftIO.Exception (SomeException, displayException, fromException, try)
+import UnliftIO (MonadUnliftIO(..), UnliftIO(..), withUnliftIO)
+import UnliftIO.Exception
+    (SomeException, catch, displayException, fromException, throwIO, try)
 
+import MergeBot.Auth (AuthParams)
 import MergeBot.Core.Monad (BotAppT, BotSettings(..), runBotAppT)
+
+{- The base monad for all servant routes -}
+
+type BaseServer api = ServerT api BaseApp
+
+newtype BaseApp a = BaseApp
+  { unBaseApp :: ReaderT (GitHubAppParams, AuthParams) IO a
+  } deriving (Functor, Applicative, Monad, MonadIO)
+
+instance MonadUnliftIO BaseApp where
+  askUnliftIO = BaseApp $
+    withUnliftIO $ \u ->
+      return $ UnliftIO (unliftIO u . unBaseApp)
+
+instance MonadError ServantErr BaseApp where
+  throwError = throwIO
+  catchError = catch
+
+runBaseApp :: GitHubAppParams -> AuthParams -> BaseApp a -> IO a
+runBaseApp ghAppParams authParams = (`runReaderT` state) . unBaseApp
+  where
+    state = (ghAppParams, authParams)
+
+-- | A helper for running BaseApp in Handler.
+runBaseHandler :: GitHubAppParams -> AuthParams -> BaseApp a -> Handler a
+runBaseHandler ghAppParams authParams = runIO . runBaseApp ghAppParams authParams
+
+getGitHubAppParams :: BaseApp GitHubAppParams
+getGitHubAppParams = BaseApp $ asks fst
+
+getAuthParams :: BaseApp AuthParams
+getAuthParams = BaseApp $ asks snd
+
+{- BotAppT helpers -}
 
 type BotApp = BotAppT IO
 
 -- | A helper around 'runBotAppT'.
-runBotApp :: Text -> BotApp a -> Token -> IO a
+runBotApp :: Text -> BotApp a -> Token -> BaseApp a
 runBotApp repo action token = do
-  GitHubAppParams{ghUserAgent, ghAppId} <- liftIO loadGitHubAppParams
+  GitHubAppParams{ghUserAgent, ghAppId} <- getGitHubAppParams
   let settings = BotSettings
         { userAgent = ghUserAgent
         , appId = ghAppId
         , ..
         }
-  runBotAppT settings action
+  liftIO $ runBotAppT settings action
   where
     (repoOwner, repoName) = parseRepo repo
 
 -- | A helper that runs the given action for every repository that the merge bot is installed on.
 --
 -- Returns a list of the results, along with the repository that produced each result.
-runBotAppForAllInstalls :: BotApp a -> IO [(Text, a)]
+runBotAppForAllInstalls :: BotApp a -> BaseApp [(Text, a)]
 runBotAppForAllInstalls action = do
-  GitHubAppParams{ghUserAgent, ghAppId, ghSigner} <- loadGitHubAppParams
+  GitHubAppParams{ghUserAgent, ghAppId, ghSigner} <- getGitHubAppParams
 
   -- get all installations
-  jwtToken <- getJWTToken ghSigner ghAppId
+  jwtToken <- liftIO $ getJWTToken ghSigner ghAppId
   let mkState token = GitHubState
         { token
         , userAgent = ghUserAgent
         , apiVersion = "machine-man-preview"
         }
       jwtState = mkState jwtToken
-  installations <- map [get| .id |] <$> runGitHubT jwtState getInstallations
+  installations <- liftIO $ map [get| .id |] <$> runGitHubT jwtState getInstallations
 
   -- run the given action in each repo
   fmap concat $ forM installations $ \installationId -> do
-    installToken <- getToken ghSigner ghAppId ghUserAgent installationId
+    installToken <- liftIO $ getToken ghSigner ghAppId ghUserAgent installationId
     let state = mkState installToken
-    repositories <- [get| .repositories[].full_name |] <$> runGitHubT state getRepositories
+    repositories <- liftIO $ [get| .repositories[].full_name |] <$> runGitHubT state getRepositories
     forM repositories $ \repo -> do
       result <- runBotApp repo action installToken
       return (repo, result)
