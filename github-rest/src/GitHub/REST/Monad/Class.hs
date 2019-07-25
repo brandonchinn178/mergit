fc00ce9a10bb006248835fce2a8208cd5318249a
Brandon Chinn <brandon@leapyear.io>
2019-07-25 13:02:36 -0700

    Add queryGitHubAll

--- github-rest/src/GitHub/REST/Monad/Class.hs
+++ github-rest/src/GitHub/REST/Monad/Class.hs
@@ -69,6 +69,15 @@ class MonadIO m => MonadGitHubREST m where
   queryGitHub :: FromJSON a => GHEndpoint -> m a
   queryGitHub = fmap fst . queryGitHubPage
 
+  queryGitHubAll :: FromJSON a => GHEndpoint -> m [a]
+  queryGitHubAll ghEndpoint = do
+    (payload, pageLinks) <- queryGitHubPage ghEndpoint
+    case pageNext pageLinks of
+      Just next -> do
+        rest <- queryGitHubAll ghEndpoint { endpoint = next, endpointVals = [] }
+        return $ payload ++ rest
+      Nothing -> return payload
+
   queryGitHub_ :: GHEndpoint -> m ()
   queryGitHub_ = void . queryGitHub @_ @Value
 
