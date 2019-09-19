fc4c6b6aa539bd3b931113cd950e05c8ceab1377
Brandon Chinn <brandon@leapyear.io>
2019-09-19 16:33:25 -0700

    Fix MonadIO in graphql-example test

--- graphql/example/test/Example/Test/Monad.hs
+++ graphql/example/test/Example/Test/Monad.hs
@@ -5,6 +5,7 @@
 
 module Example.Test.Monad where
 
+import Control.Monad.IO.Class (MonadIO)
 import Data.GraphQL (MonadQuery, mockedQuerySettings, runQueryT)
 
 import Example (App(..))
@@ -12,7 +13,7 @@ import Example.GraphQL.API (API)
 import Example.Test.Mock (MockData)
 
 newtype TestApp a = TestApp (App a)
-  deriving (Functor,Applicative,Monad,MonadQuery API)
+  deriving (Functor,Applicative,Monad,MonadIO,MonadQuery API)
 
 runMockApp :: TestApp a -> MockData -> IO a
 runMockApp (TestApp (App queryT)) mock = runQueryT (mockedQuerySettings mock) queryT
