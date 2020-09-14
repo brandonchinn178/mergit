b5a7c5a68b65f3e3cc237fcb27a1e340459f617a
Simon Swanson <simon@leapyear.io>
2020-09-14 11:54:29 -0700

    Run `terraform 0.12upgrade`
    Additional steps:
      - remove generated `versions.tf` file. It duplicates parts of `main.tf`
      - create a `.terraform-version` file for `tfenv` users to use
      - change the required version in `main.tf` to `0.12.21`

--- deploy/outputs.tf
+++ deploy/outputs.tf
@@ -1,16 +1,17 @@
 output "ip" {
-  value = "${aws_instance.merge_bot.public_ip}"
+  value = aws_instance.merge_bot.public_ip
 }
 
 output "keyfile" {
-  value = "${module.keypair.keyfile}"
+  value = module.keypair.keyfile
 }
 
 output "url" {
-  value = "${module.domain.url}"
+  value = module.domain.url
 }
 
 # SSH command without the key file
 output "ssh_cmd" {
   value = "ssh ${local.ami_user}@${aws_instance.merge_bot.public_ip}"
 }
+
