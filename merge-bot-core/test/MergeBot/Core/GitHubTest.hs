e04fc83deb1e8b310b10c7d0a590e43b02f3ee83
Brandon Chinn <brandon@leapyear.io>
2020-09-23 16:10:38 -0700

    Address feedback

--- merge-bot-core/test/MergeBot/Core/GitHubTest.hs
+++ merge-bot-core/test/MergeBot/Core/GitHubTest.hs
@@ -10,11 +10,11 @@ module MergeBot.Core.GitHubTest where
 
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.IO.Unlift (MonadUnliftIO(..))
+import Data.Aeson (Value)
 import Data.Aeson.QQ (aesonQQ)
 import Data.GraphQL (MonadGraphQLQuery)
 import Data.GraphQL.TestUtils
     (AnyResultMock, MockQueryT, ResultMock(..), mocked, runMockQueryT)
-import Data.List.Split (chunksOf)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import GitHub.Data.GitObjectID (GitObjectID(..))
@@ -35,68 +35,72 @@ test = testGroup "MergeBot.GitHub"
 testGetCICommit :: TestTree
 testGetCICommit = testGroup "getCICommit"
   [ testProperty "Loads the paginated list of parents" $ \baseBranchSHA (NonEmpty parentSHAs) ->
-      let mockSHA = GitObjectID "asdf"
-          -- parents information is paginated in groups of 5; the number here doesn't *really*
-          -- matter and doesn't *have* to match the number in getCICommit.graphql.
-          parentSHAsPages = chunksOf 5 $ baseBranchSHA : parentSHAs
-          totalPages = length parentSHAsPages
-
-          mocks = zipWith mkMock parentSHAsPages [1..]
-          mkMock parentSHAsInPage pageNum = mocked ResultMock
-            { query = GetCICommitQuery
-                { _repoOwner = testRepoOwner
-                , _repoName = testRepoName
-                , _appId = testAppId
-                , _after = if pageNum == 1 then Nothing else Just $ Text.pack $ show pageNum
-                , _sha = mockSHA
-                , _checkName = Just checkRunTry
-                }
-            , result =
-                let mkParentNode parentSHA = [aesonQQ|
-                        {
-                          "oid": #{parentSHA},
-                          "checkSuites": {
-                            "nodes": [
-                              {
-                                "checkRuns": {
-                                  "nodes": [
-                                    {
-                                      "databaseId": 1,
-                                      "name": "Bot Try"
-                                    }
-                                  ]
-                                }
-                              }
-                            ]
-                          }
-                        }
-                      |]
-                in [aesonQQ|
-                  {
-                    "repository": {
-                      "object": {
-                        "tree": {
-                          "oid": #{mockSHA},
-                          "entries": []
-                        },
-                        "status": null,
-                        "parents": {
-                          "pageInfo": {
-                            "hasNextPage": #{pageNum < totalPages},
-                            "endCursor": #{Text.pack $ show $ pageNum + 1}
-                          },
-                          "nodes": #{map mkParentNode parentSHAsInPage}
-                        }
-                      }
-                    }
-                  }
-                |]
-            }
-
-      in ioProperty $ runTestApp mocks $ do
-        CICommit{parents} <- getCICommit mockSHA CheckRunTry
-        return $ map fst parents === parentSHAs
+      forAll (chunks $ baseBranchSHA : parentSHAs) $ \parentSHAsPages ->
+        let mocks = mockGetCICommitQueries mockSHA checkRunTry parentSHAsPages
+        in ioProperty $ runTestApp mocks $ do
+          CICommit{parents} <- getCICommit mockSHA CheckRunTry
+          return $ map fst parents === parentSHAs
   ]
+  where
+    -- Generate a response to a GetCICommitQuery request.
+    mkGetCICommitResponse :: [GitObjectID] -> Int -> Int -> Value
+    mkGetCICommitResponse parentSHAsInPage pageNum totalPages =
+      let hasNextPage = pageNum < totalPages
+          endCursor = if hasNextPage then Just $ Text.pack $ show $ pageNum + 1 else Nothing
+      in [aesonQQ|
+        {
+          "repository": {
+            "object": {
+              "tree": {
+                "oid": #{mockSHA},
+                "entries": []
+              },
+              "status": null,
+              "parents": {
+                "pageInfo": {
+                  "hasNextPage": #{hasNextPage},
+                  "endCursor": #{endCursor}
+                },
+                "nodes": #{map mkGetCICommitParentNode parentSHAsInPage}
+              }
+            }
+          }
+        }
+      |]
+
+    mkGetCICommitParentNode :: GitObjectID -> Value
+    mkGetCICommitParentNode parentSHA = [aesonQQ|
+      {
+        "oid": #{parentSHA},
+        "checkSuites": {
+          "nodes": [
+            {
+              "checkRuns": {
+                "nodes": [{ "databaseId": 1 }]
+              }
+            }
+          ]
+        }
+      }
+    |]
+
+    mockGetCICommitQueries :: GitObjectID -> Text -> [[GitObjectID]] -> [AnyResultMock]
+    mockGetCICommitQueries ciCommitSHA checkName pagedParentSHAs =
+      let totalPages = length pagedParentSHAs
+          parentSHAsWithPage = zip pagedParentSHAs [1..]
+
+      in flip map parentSHAsWithPage $ \(parentSHAsInPage, pageNum) ->
+        mocked ResultMock
+          { query = GetCICommitQuery
+              { _repoOwner = testRepoOwner
+              , _repoName = testRepoName
+              , _appId = testAppId
+              , _after = if pageNum == 1 then Nothing else Just $ Text.pack $ show pageNum
+              , _sha = ciCommitSHA
+              , _checkName = Just checkName
+              }
+          , result = mkGetCICommitResponse parentSHAsInPage pageNum totalPages
+          }
 
 {- Mock data -}
 
@@ -109,7 +113,10 @@ testRepoName = "my-project"
 testAppId :: Int
 testAppId = 1
 
-{- Helpers -}
+mockSHA :: GitObjectID
+mockSHA = GitObjectID $ Text.replicate 40 "0"
+
+{- TestApp helper -}
 
 newtype TestApp a = TestApp { unTestApp :: MockQueryT IO a }
   deriving (Functor,Applicative,Monad,MonadIO,MonadGraphQLQuery)
@@ -128,6 +135,16 @@ instance MonadUnliftIO TestApp where
   -- Note: If you implement this, be sure that only one MockQueryT action is unlifted at a time.
   -- See MonadUnliftIO warnings on implementing it for StateT for more information.
   withRunInIO _ = error "MonadUnliftIO not implemented for TestApp"
+
+{- QuickCheck helpers -}
+
+-- | Arbitrarily chunk the given list.
+chunks :: [a] -> Gen [[a]]
+chunks [] = return []
+chunks xs = do
+  n <- choose (1, length xs)
+  let (chunk, rest) = splitAt n xs
+  (chunk:) <$> chunks rest
 
 {- Orphans -}
 
