57f9b4b9c74eaea667d42252330a11c9d756c499
Brandon Chinn <brandon@leapyear.io>
2020-09-24 17:37:05 -0700

    Improve deploy docs

--- deploy/outputs.tf
+++ deploy/outputs.tf
@@ -2,15 +2,10 @@ output "ip" {
   value = aws_instance.merge_bot.public_ip
 }
 
-output "keyfile" {
-  value = module.keypair.keyfile
-}
-
 output "url" {
   value = module.domain.url
 }
 
-# SSH command without the key file
 output "ssh_cmd" {
-  value = "ssh ${local.ami_user}@${aws_instance.merge_bot.public_ip}"
+  value = "ssh -i ${module.keypair.keyfile} ${local.ami_user}@${aws_instance.merge_bot.public_ip}"
 }
