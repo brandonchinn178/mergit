a283dfa2d928a55a4fa20aa8f64f962576d7b92d
Brandon Chinn <brandon@leapyear.io>
2021-02-05 13:49:02 -0800

    Realphabetize error constructors

--- merge-bot-core/src/MergeBot/Core/Error.hs
+++ merge-bot-core/src/MergeBot/Core/Error.hs
@@ -33,17 +33,17 @@ data BotError
   | CICommitMissingParents Bool Text GitObjectID
   | CommitForManyPRs GitObjectID [PullRequestId]
   | CommitLacksPR GitObjectID
-  | ConfigFileMissing [PullRequestId]
   | ConfigFileInvalid [PullRequestId] SomeException
+  | ConfigFileMissing [PullRequestId]
   | InvalidStaging [PullRequestId] Text
   | MergeConflict [PullRequestId]
   | MissingBaseBranch [PullRequestId] Text
   | MissingCheckRun GitObjectID Text
   | MissingCheckRunPR PullRequestId Text
-  | SomePRsMerged [PullRequestId] [PullRequestId]
-  | UnapprovedPR PullRequestId
-  | TreeNotUpdated [PullRequestId] PullRequestId
   | PRWasUpdatedDuringMergeRun [PullRequestId] [PullRequestId] [GitObjectID]
+  | SomePRsMerged [PullRequestId] [PullRequestId]
+  | TreeNotUpdated [PullRequestId] PullRequestId
+  | UnapprovedPR PullRequestId
 
 instance Exception BotError
 
@@ -69,15 +69,18 @@ instance Show BotError where
       ]
     CommitForManyPRs sha prs -> "Commit `" <> unOID' sha <> "` found in multiple PRs: " <> fromPRs prs
     CommitLacksPR sha -> "Commit `" <> unOID' sha <> "` does not have an associated pull request"
-    ConfigFileMissing prs -> "Merging " <> fromPRs prs <> " lacks a `" <> Text.unpack configFileName <> "` config file"
     ConfigFileInvalid prs e -> "Merging " <> fromPRs prs <> " has an invalid `" <> Text.unpack configFileName <> "` config file: " <> displayException e
+    ConfigFileMissing prs -> "Merging " <> fromPRs prs <> " lacks a `" <> Text.unpack configFileName <> "` config file"
     InvalidStaging _ branch -> "Invalid staging branch: " <> Text.unpack branch
     MergeConflict prs -> "Merge conflict: " <> fromPRs prs
     MissingBaseBranch _ branch -> "Base branch does not exist: " <> Text.unpack branch
     MissingCheckRun sha checkName -> "Commit `" <> unOID' sha <> "` missing check run named: " <> Text.unpack checkName
     MissingCheckRunPR pr checkName -> "PR #" <> show pr <> " missing check run named: " <> Text.unpack checkName
+    PRWasUpdatedDuringMergeRun _ prNums shas ->
+      case (prNums, shas) of
+        ([prNum], [sha]) -> "PR #" <> show prNum <> " was updated while the merge run was running. Expected SHA: `" <> unOID' sha <> "`"
+        _ -> "PRs " <> fromPRs prNums <> " were updated while the merge run was running."
     SomePRsMerged mergedPRs nonMergedPRs -> "PRs " <> fromPRs nonMergedPRs <> " found not merged while PRs " <> fromPRs mergedPRs <> " are merged"
-    UnapprovedPR prNum -> "PR #" <> show prNum <> " is not approved"
     TreeNotUpdated _ pr -> unlines
       [ "UNEXPECTED ERROR: Tree not updated when merging PR #" <> show pr <> "."
       , ""
@@ -85,10 +88,7 @@ instance Show BotError where
       , ""
       , "More information: https://leapyear.atlassian.net/browse/QA-178"
       ]
-    PRWasUpdatedDuringMergeRun _ prNums shas ->
-      case (prNums, shas) of
-        ([prNum], [sha]) -> "PR #" <> show prNum <> " was updated while the merge run was running. Expected SHA: `" <> unOID' sha <> "`"
-        _ -> "PRs " <> fromPRs prNums <> " were updated while the merge run was running."
+    UnapprovedPR prNum -> "PR #" <> show prNum <> " is not approved"
     where
       fromPRs = unwords . map (('#':) . show)
 
@@ -101,14 +101,14 @@ getRelevantPRs = \case
   CICommitMissingParents{} -> []
   CommitForManyPRs _ prs -> prs
   CommitLacksPR{} -> []
-  ConfigFileMissing prs -> prs
   ConfigFileInvalid prs _ -> prs
+  ConfigFileMissing prs -> prs
   InvalidStaging prs _ -> prs
   MergeConflict prs -> prs
   MissingBaseBranch prs _ -> prs
   MissingCheckRun{} -> []
   MissingCheckRunPR pr _ -> [pr]
-  SomePRsMerged mergedPRs nonMergedPRs -> mergedPRs ++ nonMergedPRs
-  UnapprovedPR pr -> [pr]
-  TreeNotUpdated allPRs _ -> allPRs
   PRWasUpdatedDuringMergeRun allPRs _ _ -> allPRs
+  SomePRsMerged mergedPRs nonMergedPRs -> mergedPRs ++ nonMergedPRs
+  TreeNotUpdated allPRs _ -> allPRs
+  UnapprovedPR pr -> [pr]
