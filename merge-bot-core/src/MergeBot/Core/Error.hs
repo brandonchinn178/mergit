3634b9579ae7844ff060eb741d5ae219cd26c5f0
Brandon Chinn <brandon@leapyear.io>
2021-03-31 13:37:40 -0700

    Build Text message directly

--- merge-bot-core/src/MergeBot/Core/Error.hs
+++ merge-bot-core/src/MergeBot/Core/Error.hs
@@ -50,49 +50,52 @@ data BotError
 instance Exception BotError
 
 getBotError :: BotError -> Text
-getBotError = Text.pack . \case
-  AmbiguousPRForCommit sha -> "Could not determine PR for commit: `" <> Text.unpack (unOID sha) <> "`"
-  BadUpdate sha prs base message -> concat
+getBotError = \case
+  AmbiguousPRForCommit sha -> "Could not determine PR for commit: `" <> unOID sha <> "`"
+  BadUpdate sha prs base message -> Text.concat
     [ "Could not merge PRs "
     , fromPRs prs
     , " into `"
-    , Text.unpack base
-    , "` (" ++ Text.unpack (unOID sha) ++ "): "
-    , Text.unpack message
+    , base
+    , "` (" <> unOID sha <> "): "
+    , message
     ]
-  CannotDetermineCheckRunPR o -> "Cannot determine PR for check run: " <> show o
-  CIBranchPushed o -> "User tried to manually create CI branch: " <> show o
-  CICommitMissingParents isStart branch sha -> concat
+  CannotDetermineCheckRunPR o -> "Cannot determine PR for check run: " <> showT o
+  CIBranchPushed o -> "User tried to manually create CI branch: " <> showT o
+  CICommitMissingParents isStart branch sha -> Text.concat
     [ "Commit `"
-    , Text.unpack (unOID sha)
+    , unOID sha
     , "` has no parents (on branch `"
-    , Text.unpack branch
+    , branch
     ,  "`) when "
     , if isStart then "starting check run" else "updating check run"
     ]
-  CommitLacksPR sha -> "Commit `" <> Text.unpack (unOID sha) <> "` does not have an associated pull request"
-  ConfigFileInvalid prs msg -> "Merging " <> fromPRs prs <> " has an invalid `" <> Text.unpack configFileName <> "` config file: " <> Text.unpack msg
-  ConfigFileMissing prs -> "Merging " <> fromPRs prs <> " lacks a `" <> Text.unpack configFileName <> "` config file"
-  InvalidStaging _ branch -> "Invalid staging branch: " <> Text.unpack branch
+  CommitLacksPR sha -> "Commit `" <> unOID sha <> "` does not have an associated pull request"
+  ConfigFileInvalid prs msg -> "Merging " <> fromPRs prs <> " has an invalid `" <> configFileName <> "` config file: " <> msg
+  ConfigFileMissing prs -> "Merging " <> fromPRs prs <> " lacks a `" <> configFileName <> "` config file"
+  InvalidStaging _ branch -> "Invalid staging branch: " <> branch
   MergeConflict prs -> "Merge conflict: " <> fromPRs prs
-  MissingBaseBranch _ branch -> "Base branch does not exist: " <> Text.unpack branch
-  MissingCheckRun sha checkName -> "Commit `" <> Text.unpack (unOID sha) <> "` missing check run named: " <> Text.unpack checkName
-  MissingCheckRunPR pr checkName -> "PR #" <> show pr <> " missing check run named: " <> Text.unpack checkName
+  MissingBaseBranch _ branch -> "Base branch does not exist: " <> branch
+  MissingCheckRun sha checkName -> "Commit `" <> unOID sha <> "` missing check run named: " <> checkName
+  MissingCheckRunPR pr checkName -> "PR #" <> showT pr <> " missing check run named: " <> checkName
   PRWasUpdatedDuringMergeRun _ prNums shas ->
     case (prNums, shas) of
-      ([prNum], [sha]) -> "PR #" <> show prNum <> " was updated while the merge run was running. Expected SHA: `" <> Text.unpack (unOID sha) <> "`"
+      ([prNum], [sha]) -> "PR #" <> showT prNum <> " was updated while the merge run was running. Expected SHA: `" <> unOID sha <> "`"
       _ -> "PRs " <> fromPRs prNums <> " were updated while the merge run was running."
   SomePRsMerged mergedPRs nonMergedPRs -> "PRs " <> fromPRs nonMergedPRs <> " found not merged while PRs " <> fromPRs mergedPRs <> " are merged"
-  TreeNotUpdated _ pr -> unlines
-    [ "UNEXPECTED ERROR: Tree not updated when merging PR #" <> show pr <> "."
+  TreeNotUpdated _ pr -> Text.unlines
+    [ "UNEXPECTED ERROR: Tree not updated when merging PR #" <> showT pr <> "."
     , ""
     , "Either the PR did not make any changes, or something went wrong on GitHub's end. Please notify the #merge-bot Slack channel and requeue your PR."
     , ""
     , "More information: https://leapyear.atlassian.net/browse/QA-178"
     ]
-  UnapprovedPR prNum -> "PR #" <> show prNum <> " is not approved"
+  UnapprovedPR prNum -> "PR #" <> showT prNum <> " is not approved"
   where
-    fromPRs = unwords . map (('#':) . show)
+    fromPRs = Text.unwords . map (Text.cons '#' . showT)
+
+    showT :: Show a => a -> Text
+    showT = Text.pack . show
 
 -- | Get the PRs that should be notified when throwing the given BotError.
 getRelevantPRs :: BotError -> [PullRequestId]
